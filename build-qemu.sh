4d09182c8645449d9831c071242979b88516a943
Yousong Zhou <yszhou4tech@gmail.com>
2015-11-13 18:06:17 +0800

    build-qemu.sh: convert build method

--- build-qemu.sh
+++ build-qemu.sh
@@ -3,75 +3,30 @@
 PKGNAME=qemu
 # N.B. prefix the version number with `v'
 VER="v2.3.0"
+
+PKG_NAME=qemu
+PKG_VERSION="2.3.0"
+PKG_SOURCE="qemu-${PKG_VERSION}.tar.bz2"
+PKG_SOURCE_URL="http://wiki.qemu-project.org/download/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM="2fab3ea4460de9b57192e5b8b311f221"
+
+. "$PWD/env.sh"
+
 # Others targets can be found in text for `--target-list` option from output of
 # `./configure --help`
 TARGETS="i386-softmmu x86_64-softmmu mipsel-softmmu mips-softmmu arm-softmmu"
-
-. "$PWD/env.sh"
-BUILD_DIR="$BASE_BUILD_DIR/$PKGNAME-$VER"
+CONFIGURE_ARGS="--target-list='$TARGETS'"
 
 # If we have git repo present, extract sources from there
 # rather than downloading them over the network.
-DIR_REPO="qemu/"
+#
+#	repo=qemu/
+#	tag=v2.3.0
+#   cd "$DIR_REPO"
+#   mkdir -p "$BUILD_DIR"
+#   git archive --format=tar "$tag" | tar -C "$BUILD_DIR" -x
 
-dirclean() {
-    rm -rf "$BUILD_DIR"
-}
-
-prepare_from_tarball() {
-    local ver="${1#v}"
-    local tar_dir="$BASE_BUILD_DIR/qemu-$ver"
-    local fn="qemu-$ver.tar.bz2"
-    local url="http://wiki.qemu-project.org/download/$fn"
-
-    # wiki.qemu-project.org does not support Range header.
-    wget -c -O "$BASE_DL_DIR/$fn" "$url"
-    tar -C "$BASE_BUILD_DIR" -xjf "$BASE_DL_DIR/$fn"
-    rm -rf "$BUILD_DIR"
-    mv -T "$tar_dir" "$BUILD_DIR"
-}
-
-prepare_from_git() {
-    local tag="$1"
-
-    cd "$DIR_REPO"
-    mkdir -p "$BUILD_DIR"
-    git archive --format=tar "$tag" | tar -C "$BUILD_DIR" -x
-}
-
-prepare_qemu() {
-    [ -x "$BUILD_DIR/configure" ] && {
-        __errmsg "$BUILD_DIR/configure already exists, skip preparing."
-        __errmsg "    manually remove it to re-prepare it."
-        return
-    }
-
-    if [ -d "$DIR_REPO/.git" ]; then
-        prepare_from_git "$VER"
-    else
-        prepare_from_tarball "$VER"
-    fi
-}
-
-build_qemu() {
-	local dest_dir="$BASE_DESTDIR/_$PKGNAME-install"
-
-    mkdir -p "$BUILD_DIR" || {
-        __errmsg "Creating build dir "$BUILD_DIR" failed."
-        return 1
-    }
-
-    cd "$BUILD_DIR"
-
-    "$BUILD_DIR/configure"            \
-        --prefix="$INSTALL_PREFIX"    \
-        --target-list="$TARGETS"      \
-
-    make -j "$NJOBS"
-	rm -rf "$dest_dir"
-    make DESTDIR="$dest_dir" install
-    cp "$dest_dir/$INSTALL_PREFIX" "$INSTALL_PREFIX"
-
+post_build() {
 	cat <<EOF
 
 To use qemu-bridge-helper, appropriate permission bits need to be set
@@ -82,6 +37,4 @@ To use qemu-bridge-helper, appropriate permission bits need to be set
 EOF
 }
 
-#dirclean
-prepare_qemu
-build_qemu
+main
