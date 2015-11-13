7f19d4d440f99fc2bddc60e0bdd5789bfc5e900a
Yousong Zhou <yszhou4tech@gmail.com>
2015-11-13 21:01:58 +0800

    build-nginx-vanilla.sh: support building extra modules

--- build-nginx-vanilla.sh
+++ build-nginx-vanilla.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+# NGINX does not support out of tree build.
+#
+#
+
 PKG_NAME=nginx
 PKG_VERSION="1.9.6"
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.gz"
@@ -8,6 +12,14 @@ PKG_SOURCE_MD5SUM="f6899825e7a8deadba4948ff84515ad6"
 
 . "$PWD/env.sh"
 
+# nginx-lua depends on LuaJIT, LuaJIT.  They has to be preinstalled.
+#
+# If they are installed on Mac OS X with MacPorts, try adding support for
+# CONFIGURE_VARS and adding the following values
+#
+#	LUAJIT_LIB=/opt/local/lib                        \
+#	LUAJIT_INC=/opt/local/include/luajit-2.0         \
+#
 CONFIGURE_ARGS='					\
 	--sbin-path=nginx				\
 	--conf-path=nginx.conf			\
@@ -22,5 +34,73 @@ install_do() {
 	cd "$PKG_BUILD_DIR"
 	make DESTDIR="$_PKG_STAGING_DIR" install
 }
+
+# master:agentzh/dns-nginx-module cannot build with NGINX 1.9.6 because of API change
+MODS_DIR="$PKG_BUILD_DIR/_mods"
+MODS='
+	master:chaoslawful/lua-nginx-module
+	master:simpl/ngx_devel_kit
+	master:agentzh/array-var-nginx-module
+	master:agentzh/echo-nginx-module
+	master:agentzh/headers-more-nginx-module
+	master:agentzh/memc-nginx-module
+	master:agentzh/rds-csv-nginx-module
+	master:agentzh/rds-json-nginx-module
+	master:agentzh/redis2-nginx-module
+	master:agentzh/set-misc-nginx-module
+	master:agentzh/xss-nginx-module
+'
+
+download_extra() {
+	local m
+	local ref repo
+	local fn source source_url
+
+	for m in $MODS; do
+		ref="${m%:*}"
+		repo="${m#*:}"
+		fn="${repo#*/}-$ref"
+		source="$BASE_DL_DIR/$fn.tar.gz"
+		source_url="https://github.com/$repo/archive/$ref.tar.gz"
+
+		if [ ! -s "$source" ]; then
+			wget -c -O "$source.dl" "$source_url"
+			mv "$source.dl" "$source"
+		fi
+	done
+}
+
+prepare_extra() {
+	local m
+	local ref repo
+	local fn source
+
+	mkdir -p "$MODS_DIR"
+	for m in $MODS; do
+		ref="${m%:*}"
+		repo="${m#*:}"
+		fn="${repo#*/}-$ref"
+		source="$BASE_DL_DIR/$fn.tar.gz"
+
+		prepare_source "$source" "$MODS_DIR" "s:^[^/]\\+:$fn:"
+	done
+}
+
+add_modules() {
+	local m
+	local ref repo
+	local fn source
+	local arg
+
+	for m in $MODS; do
+		ref="${m%:*}"
+		repo="${m#*:}"
+		fn="${repo#*/}-$ref"
+
+		arg="	--add-module=$MODS_DIR/$fn"
+		CONFIGURE_ARGS="${CONFIGURE_ARGS}${arg}"
+	done
+}
+add_modules
 
 main
