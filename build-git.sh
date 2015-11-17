db72d04ba0eb02ed47ddfcac23db6907bf6f78a9
Yousong Zhou <yszhou4tech@gmail.com>
2015-11-17 15:20:34 -0500

    build-git.sh: use $PKG_NAME in $PKG_SOURCE

--- build-git.sh
+++ build-git.sh
@@ -12,10 +12,9 @@
 #	rm -rfv /home/yousong/.usr/share/man/man3/Git*
 #	rm -rfv /home/yousong/.usr/share/perl/5.14.2/Git*
 #
-
 PKG_NAME=git
 PKG_VERSION="2.6.3"
-PKG_SOURCE="git-${PKG_VERSION}.tar.gz"
+PKG_SOURCE="${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_SOURCE_URL="https://www.kernel.org/pub/software/scm/git/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM="b711be7628a4a2c25f38d859ee81b423"
 
