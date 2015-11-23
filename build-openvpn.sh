9d94f2661a5c3ca58fe11d8e814b40236089c156
Yousong Zhou <yszhou4tech@gmail.com>
2015-11-23 14:09:21 +0800

    build-openvpn.sh: initial version 2.3.8

---
+++ build-openvpn.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+#
+# openvpn on Debian requires
+#
+#   apt-get install liblzo2-dev libpam-dev
+#
+# PKCS11 requires libpkcs11-helper-1 >= 1.11, which is not available in Debian Wheezy (only 1.09 is available)
+#
+PKG_NAME=openvpn
+PKG_VERSION="2.3.8"
+PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.xz"
+PKG_SOURCE_URL="https://swupdate.openvpn.org/community/releases/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM="acc5ea4b08ad53173784520acbd4e9c3"
+
+. "$PWD/env.sh"
+
+main
