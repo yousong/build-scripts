34dfafcb35076963622fb3ed83b962bf60a0fb4d
Yousong Zhou <yszhou4tech@gmail.com>
2015-11-30 11:49:54 +0800

    build-socat.sh: initial version 1.7.3.0

---
+++ build-socat.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+#
+# socat on Debian Wheezy 7 has version 1.7.1.3 and lacking readline support
+#
+#		sudo apt-get install libreadline-dev
+#
+PKG_NAME=socat
+PKG_VERSION=1.7.3.0
+PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.bz2"
+PKG_SOURCE_URL="http://www.dest-unreach.org/socat/download/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM="b607edb65bc6c57f4a43f06247504274"
+
+. "$PWD/env.sh"
+
+main
