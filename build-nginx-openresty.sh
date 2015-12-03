yousong/build-scripts

commit 03fa7d7e997302731267ebc136db0d8411c6bde3

env.sh: use EXTRA_xFLAGS instead of exporting them

build-nginx-openresty.sh
@@ -12,8 +12,8 @@ PKG_BUILD_DIR="$BASE_BUILD_DIR/ngx_openresty-$PKG_VERSION"
 
 # openresty is self-contained
 if os_is_darwin; then
-	export CFLAGS=""
-	export LDFLAGS=""
+	EXTRA_CFLAGS=""
+	EXTRA_LDFLAGS=""
 fi
 
 install_do() {

build-redis.sh
@@ -10,11 +10,6 @@ PKG_SOURCE_MD5SUM="c7ba233e5f92ad2f48860c815bb05480"
 
 # verbose output instead of colorful output for better logging
 MAKE_VARS="V=1"
-# redis uses lua library built by itself.
-if os_is_darwin; then
-	export CFLAGS=""
-	export LDFLAGS=""
-fi
 
 build_configure() {
 	true

env.sh
@@ -41,16 +41,16 @@ _init() {
 
     alias cp="cp -a -T"
 	export PKG_CONFIG_PATH="$INSTALL_PREFIX/lib/pkgconfig:$INSTALL_PREFIX/share/pkgconfig"
-	export CPPFLAGS="-I$INSTALL_PREFIX/include"
-	export CFLAGS="-I$INSTALL_PREFIX/include"
-	export LDFLAGS="-Wl,-rpath,$INSTALL_PREFIX/lib"
+	EXTRA_CPPFLAGS="-I$INSTALL_PREFIX/include"
+	EXTRA_CFLAGS="-I$INSTALL_PREFIX/include"
+	EXTRA_LDFLAGS="-L$INSTALL_PREFIX/lib -Wl,-rpath,$INSTALL_PREFIX/lib"
 	if os_is_darwin; then
 		MACPORTS_PREFIX="${MACPORTS_PREFIX:-/opt/local}"
 		export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$MACPORTS_PREFIX/lib/pkgconfig"
 		export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$MACPORTS_PREFIX/share/pkgconfig"
-		export CPPFLAGS="$CPPFLAGS -I$MACPORTS_PREFIX/include"
-		export CFLAGS="$CFLAGS -I$MACPORTS_PREFIX/include"
-		export LDFLAGS="$LDFLAGS -Wl,-rpath,$MACPORTS_PREFIX/lib"
+		EXTRA_CPPFLAGS="$EXTRA_CPPFLAGS -I$MACPORTS_PREFIX/include"
+		EXTRA_CFLAGS="$EXTRA_CFLAGS -I$MACPORTS_PREFIX/include"
+		EXTRA_LDFLAGS="$EXTRA_LDFLAGS -L$MACPORTS_PREFIX/lib -Wl,-rpath,$MACPORTS_PREFIX/lib"
 	fi
 	NJOBS="${NJOBS:-$((2 * $(ncpus)))}"
 }
@@ -167,7 +167,10 @@ build_pre() {
 
 build_configure_default() {
 	cd "$PKG_BUILD_DIR"
-	eval "$CONFIGURE_VARS"				\
+	eval CPPFLAGS="'$EXTRA_CPPFLAGS'"	\
+		 CFLAGS="'$EXTRA_CFLAGS'"		\
+		 LDFLAGS="'$EXTRA_LDFLAGS'"		\
+		 "$CONFIGURE_VARS"				\
 		 "$PKG_BUILD_DIR/configure"		\
 			--prefix="$INSTALL_PREFIX"	\
 			"$CONFIGURE_ARGS"
@@ -179,9 +182,9 @@ build_configure_cmake() {
 	eval cmake												\
 		-DCMAKE_BUILD_TYPE=Release							\
 		-DCMAKE_INSTALL_PREFIX="'$INSTALL_PREFIX'"			\
-		-DCMAKE_EXE_LINKER_FLAGS="'$LDFLAGS'"				\
-		-DCMAKE_SHARED_LINKER_FLAGS="'$LDFLAGS'"			\
-		-DCMAKE_C_FLAGS="'$CFLAGS'"							\
+		-DCMAKE_EXE_LINKER_FLAGS="'$EXTRA_LDFLAGS'"			\
+		-DCMAKE_SHARED_LINKER_FLAGS="'$EXTRA_LDFLAGS'"		\
+		-DCMAKE_C_FLAGS="'$EXTRA_CFLAGS'"					\
 		-DCMAKE_MACOSX_RPATH=on								\
 		$CMAKE_ARGS
 }

commit 3fcd67cbbe6b3d48a89a84c47e1d610f19e9f5ad

build-redis.sh: fix building redis on OSX
It's a regression caused by commit "03fa7d7 env.sh: use EXTRA_xFLAGS
instead of exporting them".

While at it, also make EXTRA_CPPFLAGS a blank definition for openrestry.

build-nginx-openresty.sh
@@ -12,6 +12,7 @@ PKG_BUILD_DIR="$BASE_BUILD_DIR/ngx_openresty-$PKG_VERSION"
 
 # openresty is self-contained
 if os_is_darwin; then
+	EXTRA_CPPFLAGS=""
 	EXTRA_CFLAGS=""
 	EXTRA_LDFLAGS=""
 fi

build-redis.sh
@@ -10,6 +10,12 @@ PKG_SOURCE_MD5SUM="c7ba233e5f92ad2f48860c815bb05480"
 
 # verbose output instead of colorful output for better logging
 MAKE_VARS="V=1"
+# openresty is self-contained
+if os_is_darwin; then
+	EXTRA_CPPFLAGS=""
+	EXTRA_CFLAGS=""
+	EXTRA_LDFLAGS=""
+fi
 
 build_configure() {
 	true