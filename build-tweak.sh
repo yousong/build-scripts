61f1abc5d7ce7d5e9dc3a252a263a6d7c7dd1655
Yousong Zhou <yszhou4tech@gmail.com>
2015-12-14 11:21:46 +0800

    tweak: initial version 2015-04-22

---
+++ build-tweak.sh
@@ -0,0 +1,55 @@
+#!/bin/sh -e
+
+PKG_NAME=tweak
+PKG_VERSION=2015-04-22
+PKG_SOURCE_VERSION=18448721678b2169a4e3cc03c048f8fb85ee7776
+PKG_SOURCE="$PKG_NAME-${PKG_VERSION}-${PKG_SOURCE_VERSION}.tar.gz"
+PKG_SOURCE_URL="http://tartarus.org/~simon-git/gitweb/?p=tweak.git;a=snapshot;h=$PKG_SOURCE_VERSION;sf=tgz"
+PKG_SOURCE_MD5SUM=
+
+. "$PWD/env.sh"
+
+prepare_source() {
+	local dir="$(basename $PKG_BUILD_DIR)"
+	untar "$BASE_DL_DIR/$PKG_SOURCE" "$BASE_BUILD_DIR" "s:^[^/]\\+:$dir:"
+}
+
+build_configure() {
+	true
+}
+
+MAKE_VARS="LFLAGS='$EXTRA_LDFLAGS' PREFIX='$_PKG_STAGING_DIR$INSTALL_PREFIX'"
+
+do_patch() {
+    cd "$PKG_BUILD_DIR"
+	patch -p1 <<"EOF"
+--- a/Makefile.orig	2015-12-14 11:13:57.000000000 +0800
++++ b/Makefile	2015-12-14 11:17:00.000000000 +0800
+@@ -24,7 +24,7 @@ LIBS := 
+ 
+ PREFIX=$(DESTDIR)/usr/local
+ BINDIR=$(PREFIX)/bin
+-MANDIR=$(PREFIX)/man/man1
++MANDIR=$(PREFIX)/share/man/man1
+ 
+ TWEAK := main.o keytab.o actions.o search.o rcfile.o buffer.o btree.o
+ 
+@@ -38,13 +38,10 @@ LIBS += -lncurses
+ TWEAK += curses.o
+ endif
+ 
+-.c.o:
+-	$(CC) $(CFLAGS) $*.c
+-
+ all: tweak tweak.1 btree.html
+ 
+ tweak:	$(TWEAK)
+-	$(LINK) -o tweak $(TWEAK) $(LIBS)
++	$(LINK) $(LFLAGS) -o tweak $(TWEAK) $(LIBS)
+ 
+ tweak.1:  manpage.but
+ 	halibut --man=$@ $<
+EOF
+}
+
+main
