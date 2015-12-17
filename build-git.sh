dfb4c7e6ae0bee2dc297ffc1b35e5b5b1f0c8357
Yousong Zhou <yszhou4tech@gmail.com>
2015-12-17 21:03:06 +0800

    build-git.sh: add notes on http, https transport support
    Currently configure script from git tarball won't error even we
    configure explicitly --with-curl --with-openssl

--- build-git.sh
+++ build-git.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -e
 
-# Extra packages has to be installed to support https helper etc.
+# Extra packages has to be installed to support http,https transport (git-http-backend, etc.)
 #
-#   apt-get install -y curl-devel
+#   sudo apt-get install -y libcurl4-openssl-dev libssl-dev
+#   sudo yum install -y curl-devel openssl-devel
 #
 #   yum -y groupinstall "Development Tools"
 #   yum-builddep git-email
 #
 # Sometimes we need to install several perl modules for the git-send-email to work
 #
+#	sudo yum install -y perl-CPAN
 #	sudo cpan Net::SMTP::SSL MIME::Base64 Authen::SASL
 #
 # Manpages and perl bindings are installed with readonly permissions 0444.  To overwrite previous install, clean them
