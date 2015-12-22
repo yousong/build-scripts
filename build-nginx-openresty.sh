7617e6ed912a997711d256f90ddceb1e64bbcc2e
Yousong Zhou <yszhou4tech@gmail.com>
2015-12-22 20:58:23 +0800

    openresty: patch nginx-1.9.3/auto/feature for detecting Lua headers
    ngx_lua requires the lua version to be exactly 5.1.  OpenResty comes
    with its own LuaJIT build and should be used in the build.  Put the
    inc and ld options at the front to prevent possible detection failure
    caused by different versions of Lua already installed to other places

--- build-nginx-openresty.sh
+++ build-nginx-openresty.sh
@@ -10,3 +10,23 @@ PKG_SOURCE_MD5SUM=cde1f7127f6ba413ee257003e49d6d0a
 PKG_BUILD_DIR="$BASE_BUILD_DIR/ngx_openresty-$PKG_VERSION"
 
 . "$PWD/utils-nginx.sh"
+
+do_patch() {
+    cd "$PKG_BUILD_DIR"
+
+	patch -p0 <<"EOF"
+--- bundle/nginx-1.9.3/auto/feature.orig	2015-12-22 20:52:59.000000000 +0800
++++ bundle/nginx-1.9.3/auto/feature	2015-12-22 20:53:37.000000000 +0800
+@@ -39,8 +39,8 @@ int main() {
+ END
+ 
+ 
+-ngx_test="$CC $CC_TEST_FLAGS $CC_AUX_FLAGS $ngx_feature_inc_path \
+-          -o $NGX_AUTOTEST $NGX_AUTOTEST.c $NGX_TEST_LD_OPT $ngx_feature_libs"
++ngx_test="$CC $ngx_feature_inc_path $CC_TEST_FLAGS $CC_AUX_FLAGS \
++          -o $NGX_AUTOTEST $NGX_AUTOTEST.c $ngx_feature_libs $NGX_TEST_LD_OPT"
+ 
+ ngx_feature_inc_path=
+ 
+EOF
+}
