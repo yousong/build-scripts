2a5c0736f2440d28184bd9b7226483a4b2dcfdcd
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-03 02:22:33 +0800

    apr: initial version 1.5.2

---
+++ build-apr.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+#
+# APR for Apache Portable Runtime
+#
+PKG_NAME=apr
+PKG_VERSION=1.5.2
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_SOURCE_URL="http://www.us.apache.org/dist//apr/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=4e9769f3349fe11fc0a5e1b224c236aa
+
+. "$PWD/env.sh"
