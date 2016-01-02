9b3cabfee24fb59bee26b373596a9d958dc63d17
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-03 00:55:51 +0800

    tengine: bump to version 2.1.2 and enable modules

--- build-nginx-tengine.sh
+++ build-nginx-tengine.sh
@@ -1,10 +1,16 @@
 #!/bin/sh -e
 
 PKG_NAME=tengine
-PKG_VERSION=2.1.0
+PKG_VERSION=2.1.2
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://tengine.taobao.org/download/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=fb60c57c2610c6a356153613c485e4af
+PKG_SOURCE_MD5SUM=76af6a4969e7179c2ff4512d31d9e12d
 
 . "$PWD/env.sh"
 . "$PWD/utils-nginx.sh"
+
+CONFIGURE_ARGS="$CONFIGURE_ARGS		\\
+	--with-http_ssl_module			\\
+	--with-http_mp4_module			\\
+	--with-http_v2_module			\\
+"
