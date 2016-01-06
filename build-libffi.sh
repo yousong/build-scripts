1a57afcbb470d3a14b50649b3c0ad9da7670dffc
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-06 17:09:06 +0800

    libffi: initial version 3.2.1

---
+++ build-libffi.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+#
+PKG_NAME=libffi
+PKG_VERSION=3.2.1
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="ftp://sourceware.org/pub/libffi/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=83b89587607e3eb65c70d361f13bab43
+
+. "$PWD/env.sh"
