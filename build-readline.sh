30316461c2f9791aec3e25d8a22224d716bf97a2
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-07 12:17:59 +0800

    readline: initial version 6.3

---
+++ build-readline.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+#
+PKG_NAME=readline
+PKG_VERSION=6.3
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="ftp://ftp.cwru.edu/pub/bash/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=33c8fb279e981274f485fd91da77e94a
+
+. "$PWD/env.sh"
