3f41539cc5530cf31d2e68ee72ca66b21ae81902
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-09 20:25:28 +0800

    pcre: initial version 8.38

---
+++ build-pcre.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+#
+PKG_NAME=pcre
+PKG_VERSION=8.38
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_SOURCE_URL="ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=00aabbfe56d5a48b270f999b508c5ad2
+
+. "$PWD/env.sh"
