9bf1f42d8278bd5df441359cd5545935d7f9c5d1
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-11 00:32:42 +0800

    python2: patch for building pyexpat on Mac OS X
    Same patch as the one for python3 here.

--- build-python2.sh
+++ build-python2.sh
@@ -12,3 +12,44 @@ CONFIGURE_ARGS='				\
 	--enable-unicode=ucs4		\
 	--with-ensurepip=upgrade	\
 '
+
+do_patch() {
+	if ! os_is_darwin; then
+		return 0
+	fi
+
+    cd "$PKG_BUILD_DIR"
+
+	# for building pyexpat module
+	# taken from: https://github.com/LibreOffice/core/blob/master/external/python3/python-3.3.5-pyexpat-symbols.patch.1
+	patch -p0 <<"EOF"
+HACK: Fix build breakage on MacOS:
+
+*** WARNING: renaming "pyexpat" since importing it failed: dlopen(build/lib.macosx-10.6-i386-3.3/pyexpat.so, 2): Symbol not found: _XML_ErrorString
+
+This reverts c242a8f30806 from the python hg repo:
+
+restore namespacing of pyexpat symbols (closes #19186)
+
+
+See http://bugs.python.org/issue19186#msg214069
+
+The recommendation to include Modules/inc at first broke the Linux build...
+
+So do it this way, as it was before. Needs some realignment later.
+
+--- Modules/expat/expat_external.h
++++ Modules/expat/expat_external.h
+@@ -7,10 +7,6 @@
+
+ /* External API definitions */
+
+-/* Namespace external symbols to allow multiple libexpat version to
+-   co-exist. */
+-#include "pyexpatns.h"
+-
+ #if defined(_MSC_EXTENSIONS) && !defined(__BEOS__) && !defined(__CYGWIN__)
+ #define XML_USE_MSC_EXTENSIONS 1
+ #endif
+EOF
+}
