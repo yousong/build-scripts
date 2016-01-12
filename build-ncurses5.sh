b6efbb8aa07ba0ad3d708f692fc57648e842aaab
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-12 13:39:10 +0800

    ncurses5: symlinks for normal libraries, apply patches, configure args

--- build-ncurses5.sh
+++ build-ncurses5.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 #
+# - See section "Growth of the feature set",
+#   http://invisible-island.net/ncurses/ncurses.faq.html
+#
 PKG_NAME=ncurses5
 PKG_VERSION=5.9
 PKG_SOURCE="ncurses-$PKG_VERSION.tar.gz"
@@ -10,50 +13,37 @@ PKG_SOURCE_MD5SUM=8cb9c412e5f2d96bc6f459aa8c6282a1
 PKG_BUILD_DIR="$BASE_BUILD_DIR/ncurses-$PKG_VERSION"
 PKG_STAGING_DIR="$BASE_DESTDIR/ncurses-$PKG_VERSION-install"
 
+PATCHDATE=20141206
+PATCHBALL="ncurses-$PKG_VERSION-patch-$PATCHDATE.sh.gz"
+PATCHURL="ftp://invisible-island.net/ncurses/$PKG_VERSION/patch-$PKG_VERSION-$PATCHDATE.sh.gz"
+
+fetch_patches() {
+	local l
+
+	download_http "$PATCHBALL" "$PATCHURL" "170392a335809136d8fb6ba175ee1dba"
+}
+
+apply_patches() {
+	local f
+
+	cd "$PKG_BUILD_DIR"
+
+	if [ -f ".patched" ]; then
+		__errmsg "$PKG_BUILD_DIR/.patched exists, skip patching."
+		return 0
+	fi
+
+	gunzip -c "$BASE_DL_DIR/$PATCHBALL" | sh
+	touch .patched
+}
+
 do_patch() {
 	cd "$PKG_BUILD_DIR"
 
+	fetch_patches
+	apply_patches
+
 	patch -p0 <<"EOF"
---- c++/cursesf.h.orig	2011-07-23 10:05:34.000000000 +0200
-+++ c++/cursesf.h	2011-07-23 10:06:20.000000000 +0200
-@@ -677,7 +677,7 @@
-   }
- 
- public:
--  NCursesUserForm (NCursesFormField Fields[],
-+  NCursesUserForm (NCursesFormField* Fields[],
- 		   const T* p_UserData = STATIC_CAST(T*)(0),
- 		   bool with_frame=FALSE,
- 		   bool autoDelete_Fields=FALSE)
-@@ -686,7 +686,7 @@
- 	set_user (const_cast<void *>(p_UserData));
-   };
- 
--  NCursesUserForm (NCursesFormField Fields[],
-+  NCursesUserForm (NCursesFormField* Fields[],
- 		   int nlines,
- 		   int ncols,
- 		   int begin_y = 0,
---- c++/cursesm.h.orig	2011-07-23 10:05:50.000000000 +0200
-+++ c++/cursesm.h	2011-07-23 10:06:46.000000000 +0200
-@@ -635,7 +635,7 @@
-   }
- 
- public:
--  NCursesUserMenu (NCursesMenuItem Items[],
-+  NCursesUserMenu (NCursesMenuItem* Items[],
- 		   const T* p_UserData = STATIC_CAST(T*)(0),
- 		   bool with_frame=FALSE,
- 		   bool autoDelete_Items=FALSE)
-@@ -644,7 +644,7 @@
- 	set_user (const_cast<void *>(p_UserData));
-   };
- 
--  NCursesUserMenu (NCursesMenuItem Items[],
-+  NCursesUserMenu (NCursesMenuItem* Items[],
- 		   int nlines,
- 		   int ncols,
- 		   int begin_y = 0,
 EOF
 }
 
@@ -61,22 +51,23 @@ EOF
 EXTRA_CPPFLAGS=
 EXTRA_CFLAGS=
 EXTRA_LDFLAGS="-L$INSTALL_PREFIX/lib -Wl,-rpath,$INSTALL_PREFIX/lib"
-CONFIGURE_VARS="										\\
-	PKG_CONFIG_LIBDIR='$INSTALL_PREFIX/lib/pkgconfig'	\\
-"
+# - --with-termlib, generate separate tinfo library
 # - enable building shared libraries
-# - generate normal manpages instead of those with .cx ext
 # - suppress check for ada95
-# - suppress check for ada95
+# - dont generate debug-libraries (those ending with _g)
 # - compile with wide-char/UTF-8 code
 # - --enable-overwrite,
 # - compile in termcap fallback support
 # - compile with SIGWINCH handler
 CONFIGURE_ARGS="						\\
-	--with-terminfo						\\
+	--with-termlib						\\
 	--with-shared						\\
+	--with-cxx-shared					\\
+	--with-normal						\\
 	--with-manpage-format=normal		\\
+	--with-pkg-config-libdir='$INSTALL_PREFIX/lib/pkgconfig'	\\
 	--without-ada						\\
+	--without-debug						\\
 	--enable-widec						\\
 	--enable-overwrite					\\
 	--enable-termcap					\\
@@ -84,3 +75,29 @@ CONFIGURE_ARGS="						\\
 	--enable-pc-files					\\
 	--mandir=$INSTALL_PREFIX/share/man	\\
 "
+# we cannot do autoreconf because AC_DIVERT_HELP may not be universally
+# available
+
+staging_post() {
+	local major="${PKG_VERSION%%.*}"
+	local f based="$PKG_STAGING_DIR/$INSTALL_PREFIX"
+	local suf sufm
+
+	if os_is_linux; then
+		suf=so
+		sufm="so.${major}"
+	else
+		suf=dylib
+		sufm="${major}.dylib"
+	fi
+
+	# link from normal version to the wchar version
+	for f in form menu panel ncurses ncurses++ tinfo; do
+		ln -s "lib${f}w.$sufm" "$based/lib/lib${f}.$suf"
+		ln -s "lib${f}w.$sufm" "$based/lib/lib${f}.$sufm"
+		ln -s "lib${f}w.a" "$based/lib/lib${f}.a"
+		ln -s "${f}w.pc" "$based/lib/pkgconfig/${f}.pc"
+	done
+	ln -s "libncurses.$sufm" $based/lib/libtermcap.$suf
+	ln -s "ncursesw${major}-config" "$based/bin/ncurses${major}-config"
+}
