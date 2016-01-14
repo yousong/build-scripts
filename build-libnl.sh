316037a5240246b2859792caa339da8bdcb95a51
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-14 21:53:27 +0800

    libnl: initial version 3.2.25

---
+++ build-libnl.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+#
+PKG_NAME=libnl
+PKG_VERSION=3.2.25
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://www.infradead.org/~tgr/libnl/files/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=03f74d0cd5037cadc8cdfa313bbd195c
+PKG_PLATFORM=linux
+
+. "$PWD/env.sh"
