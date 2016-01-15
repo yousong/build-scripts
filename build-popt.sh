e7273e902b1edb134cbe05c340690a7eae4482b2
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-15 18:10:02 +0800

    popt: initial version 1.16

---
+++ build-popt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+#
+PKG_NAME=popt
+PKG_VERSION=1.16
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="http://rpm5.org/files/popt/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=3743beefa3dd6247a73f8f7a32c14c33
+
+. "$PWD/env.sh"
