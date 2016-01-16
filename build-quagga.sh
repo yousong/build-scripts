8b1131b7af0e0b56d20abc41ffbee8a58a26e157
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-16 17:50:18 +0800

    quagga: add notes about deps and autoconf fixup

--- build-quagga.sh
+++ build-quagga.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 #
+# configure requires gawk
+#
 PKG_NAME=quagga
 PKG_VERSION=0.99.24
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
@@ -10,5 +12,7 @@ PKG_SOURCE_MD5SUM=f1dce9efba8d1ca359f2998f4575206d
 
 configure_pre() {
 	cd "$PKG_BUILD_DIR"
+	# this is required otherwise configure script may complain that it
+	# counldn't find install-sh or such things
 	autoconf_fixup
 }
