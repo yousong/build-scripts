e955a28eddee873099cf2cc2184df6a12c637f4d
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-17 21:07:11 +0800

    libnet: initial version 1.1.6

---
+++ build-libnet.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+PKG_NAME=libnet
+PKG_VERSION=1.1.6
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://downloads.sourceforge.net/libnet-dev/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=710296fe424a49344e5fcc0d09e53317
+
+. "$PWD/env.sh"
