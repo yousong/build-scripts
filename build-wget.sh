790b15188f95579020e51678ff6b1d24f0e16fa7
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-18 14:25:22 +0800

    wget: initial version 1.17.1

---
+++ build-wget.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+#
+PKG_NAME=wget
+PKG_VERSION=1.17.1
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SOURCE_URL="http://ftp.gnu.org/gnu/wget/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=b0d58ef4963690e71effba24c105ed52
+PKG_DEPENDS='libiconv openssl pcre zlib'
+
+. "$PWD/env.sh"
+
+# Wget defaults to GNU TLS but that requires too many dependencies
+CONFIGURE_ARGS='		\
+	--with-ssl=openssl	\
+'
