4b0e33c0794d20893a2492f3d0da7103bd50d6ea
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-18 14:24:47 +0800

    zlib: initial version 1.2.8

---
+++ build-zlib.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+#
+PKG_NAME=zlib
+PKG_VERSION=1.2.8
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SOURCE_URL="http://zlib.net/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=28f1205d8dd2001f26fec1e8c2cebe37
+
+. "$PWD/env.sh"
