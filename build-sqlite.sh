4427e3f5c7eea3d7640231b7af6cc09c2e74b422
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-20 23:25:18 +0800

    sqlite: initial version autoconf-3100100

---
+++ build-sqlite.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+#
+PKG_NAME=sqlite
+PKG_VERSION=autoconf-3100100
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://www.sqlite.org/2016/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=f315a86cb3e8671fe473baa8d34746f6
+PKG_DEPENDS=ncurses
+
+. "$PWD/env.sh"
+
+# it's tgetent() or readline() it wants, not the libreadline library
+CONFIGURE_ARGS='			\
+	--enable-readline		\
+	--enable-json1			\
+'
