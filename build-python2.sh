ad8407fbf898e6be3d21bddfb510a3d7b29af722
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-20 23:24:56 +0800

    python{2,3}: add dependencies

--- build-python2.sh
+++ build-python2.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -e
 #
 PKG_NAME=python2
-PKG_VERSION="2.7.10"
+PKG_VERSION=2.7.10
 PKG_SOURCE="Python-${PKG_VERSION}.tar.xz"
 PKG_SOURCE_URL="https://www.python.org/ftp/python/$PKG_VERSION/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM="c685ef0b8e9f27b5e3db5db12b268ac6"
+PKG_SOURCE_MD5SUM=c685ef0b8e9f27b5e3db5db12b268ac6
+PKG_DEPENDS='openssl ncurses readline sqlite zlib'
 
 . "$PWD/env.sh"
 PKG_BUILD_DIR="$BASE_BUILD_DIR/Python-$PKG_VERSION"
