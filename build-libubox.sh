4d6addcf2d82c6fe2ee35df023ce45d0a5ca200e
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-23 12:31:53 +0800

    libubox: restore building lua related code

--- build-libubox.sh
+++ build-libubox.sh
@@ -12,6 +12,7 @@ PKG_SOURCE_VERSION=10429bccd0dc5d204635e110a7a8fae7b80d16cb
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}-${PKG_SOURCE_VERSION}.tar.gz"
 PKG_SOURCE_URL="http://git.openwrt.org/?p=project/libubox.git;a=snapshot;h=$PKG_SOURCE_VERSION;sf=tgz"
 PKG_SOURCE_UNTAR_FIXUP=1
+PKG_DEPENDS='json-c lua5.1'
 PKG_CMAKE=1
 
 . "$PWD/env.sh"
@@ -19,45 +20,17 @@ PKG_CMAKE=1
 do_patch() {
 	cd "$PKG_BUILD_DIR"
 
-	if ! os_is_darwin; then
-		return 0
-	fi
-	patch -p1 <<"EOF"
-From b1ace03730b456fe890bd35ddb61f10990f9ebfe Mon Sep 17 00:00:00 2001
-From: Yousong Zhou <yszhou4tech@gmail.com>
-Date: Tue, 11 Nov 2014 08:03:07 +0800
-Subject: [PATCH] cmake: fix build with MacPorts.
-
-Signed-off-by: Yousong Zhou <yszhou4tech@gmail.com>
----
- lua/CMakeLists.txt | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/lua/CMakeLists.txt b/lua/CMakeLists.txt
-index 10c6dc1..bd8164b 100644
---- a/lua/CMakeLists.txt
-+++ b/lua/CMakeLists.txt
-@@ -8,7 +8,7 @@ IF(NOT LUA_CFLAGS)
- 	FIND_PROGRAM(PKG_CONFIG pkg-config)
- 	IF(PKG_CONFIG)
- 		EXECUTE_PROCESS(
--			COMMAND pkg-config --silence-errors --cflags lua5.1
-+			COMMAND pkg-config --silence-errors --cflags lua-5.1
- 			OUTPUT_VARIABLE LUA_CFLAGS
- 			OUTPUT_STRIP_TRAILING_WHITESPACE
- 		)
--- 
-2.2.1
+	patch -p0 <<"EOF"
+--- lua/CMakeLists.txt.orig	2016-01-23 12:14:59.000000000 +0800
++++ lua/CMakeLists.txt	2016-01-23 12:22:35.000000000 +0800
+@@ -15,7 +15,7 @@ IF(NOT LUA_CFLAGS)
+ 	ENDIF()
+ ENDIF()
+ 
+-ADD_DEFINITIONS(-Os -Wall -Werror --std=gnu99 -g3 -I.. ${LUA_CFLAGS})
++SET(CMAKE_C_FLAGS "-I.. ${LUA_CFLAGS} ${CMAKE_C_FLAGS} -Os -Wall -Werror --std=gnu99 -g3")
+ LINK_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/..)
+ 
+ IF(APPLE)
 EOF
 }
-
-# XXX: on Debian Wheezy, json.h is in directory /usr/include/json/ and cannot
-# be found by examples code
-CMAKE_ARGS="-DBUILD_EXAMPLES=no"
-# XXX: MacPorts currently installs header files of lua-5.3 to /opt/local/include
-# directory instead of a subdirectory 5.3/ there like the 5.1/ does.  In this
-# case the incompatible 5.3 headers will be used and compilation error happens
-if os_is_darwin; then
-	CMAKE_ARGS="$CMAKE_ARGS -DBUILD_LUA=no"
-fi
-
