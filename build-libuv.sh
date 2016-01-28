8c72998c38a9159cbb6bc531dc1b105e84c4df81
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-28 16:21:53 +0800

    libuv: initial version 1.8.0

---
+++ build-libuv.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+PKG_NAME=libuv
+PKG_VERSION=1.8.0
+PKG_SOURCE="$PKG_NAME-v${PKG_VERSION}.tar.gz"
+PKG_SOURCE_URL="http://dist.libuv.org/dist/v$PKG_SOURCE/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=f4229c4360625e973ae933cb92e1faf7
+PKG_SOURCE_UNTAR_FIXUP=1
+
+. "$PWD/env.sh"
+
+configure_pre() {
+	cd "$PKG_BUILD_DIR"
+	sh -e autogen.sh
+}
