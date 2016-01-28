0dc3ee3f8bb5b422dd08ac12f0bc1c4694673483
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-28 16:21:29 +0800

    wslay: inital version 1.0.0

---
+++ build-wslay.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+PKG_NAME=wslay
+PKG_VERSION=1.0.0
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_SOURCE_URL="https://downloads.sourceforge.net/wslay/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=e1002c08f03d939e93d2e2ec1b44a425
+
+. "$PWD/env.sh"
