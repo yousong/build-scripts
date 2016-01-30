c209aecbbbbc3647c28bb3a554e2cef0f5532eed
Yousong Zhou <yszhou4tech@gmail.com>
2016-01-31 03:24:12 +0800

    stunnel: initial version 5.30

---
+++ build-stunnel.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+PKG_NAME=stunnel
+PKG_VERSION=5.30
+PKG_SOURCE="stunnel-${PKG_VERSION}.tar.gz"
+PKG_SOURCE_URL="https://www.stunnel.org/downloads/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=7bbf27296a83c0b752f6bb6d1b750b19
+PKG_DEPENDS='openssl zlib'
+
+. "$PWD/env.sh"
