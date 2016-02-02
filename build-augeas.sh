67966077fbc3aed34ab3b4600cfb9ab6d1202503
Yousong Zhou <yszhou4tech@gmail.com>
2016-02-02 20:03:45 +0800

    augeas: depend on readline

--- build-augeas.sh
+++ build-augeas.sh
@@ -1,9 +1,16 @@
 #!/bin/sh -e
 #
+# libxml-2.0 is required
+#
+#	sudo yum install libxml2-devel
+#
+# augtool requires readline
+#
 PKG_NAME=augeas
 PKG_VERSION=1.4.0
 PKG_SOURCE="${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_SOURCE_URL="http://download.augeas.net/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=a2536a9c3d744dc09d234228fe4b0c93
+PKG_DEPENDS=readline
 
 . "$PWD/env.sh"
