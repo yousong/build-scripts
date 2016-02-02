425e0522666eae5c363c608de807604ddba65e83
Yousong Zhou <yszhou4tech@gmail.com>
2016-02-02 20:00:35 +0800

    xz: initial version 5.2.2

---
+++ build-xz.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+#
+PKG_NAME=xz
+PKG_VERSION=5.2.2
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="http://tukaani.org/xz/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=7cf6a8544a7dae8e8106fdf7addfa28c
+
+. "$PWD/env.sh"
