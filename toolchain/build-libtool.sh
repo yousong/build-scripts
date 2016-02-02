677e61f9f997ca00c690c8e1e49c6c3f56f6f32e
Yousong Zhou <yszhou4tech@gmail.com>
2016-02-02 20:09:58 +0800

    libtool: initial version 2.4

---
+++ toolchain/build-libtool.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+PKG_NAME=libtool
+PKG_VERSION=2.4
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SOURCE_URL="http://ftpmirror.gnu.org/libtool/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=4e6144439d95d7332dc50ace6dd24c55
+
+. "$PWD/env.sh"
