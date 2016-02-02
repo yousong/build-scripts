f0389929e5619c3d1fb02af69b511114c977723d
Yousong Zhou <yszhou4tech@gmail.com>
2016-02-02 20:10:28 +0800

    ruby: initial version 2.3.0

---
+++ toolchain/build-ruby.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+PKG_NAME=ruby
+PKG_VERSION=2.3.0
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://cache.ruby-lang.org/pub/ruby/${PKG_VERSION%.*}/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=e81740ac7b14a9f837e9573601db3162
+
+. "$PWD/env.sh"
