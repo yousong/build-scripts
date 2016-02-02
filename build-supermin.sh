759e952f731f10945e6dfe8e2e8327cd6eb20aae
Yousong Zhou <yszhou4tech@gmail.com>
2016-02-02 20:07:54 +0800

    supermin: depend on e2fsprogs

--- build-supermin.sh
+++ build-supermin.sh
@@ -3,6 +3,7 @@
 # supermin dependency
 #
 #	sudo apt-get install ocaml ocaml-findlib ext2fs-dev
+#	sudo yum install ocaml ocaml-findlib e2fsprogs-devel glibc-static
 #
 PKG_NAME=supermin
 PKG_VERSION=5.1.13
@@ -10,5 +11,6 @@ PKG_SOURCE="${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_SOURCE_URL="http://libguestfs.org/download/supermin/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=e39f95b07651b25d310687f4407b8466
 PKG_PLATFORM=linux
+PKG_DEPENDS=e2fsprogs
 
 . "$PWD/env.sh"
