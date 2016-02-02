c668a7274a64914aba2a618db4c3e3f55ba9ba9a
Yousong Zhou <yszhou4tech@gmail.com>
2016-02-02 20:00:44 +0800

    lzo: initial version 2.09

---
+++ build-lzo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+PKG_NAME=lzo
+PKG_VERSION=2.09
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="http://www.oberhumer.com/opensource/lzo/download/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=c7ffc9a103afe2d1bba0b015e7aa887f
+
+. "$PWD/env.sh"
