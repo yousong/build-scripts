ef8d8b1d1962891f4c23edd0f448c9f986c0f0c5
Yousong Zhou <yszhou4tech@gmail.com>
2016-02-04 20:13:26 +0800

    shadowsocks-libev: initial version 2.4.5

---
+++ build-shadowsocks-libev.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+PKG_NAME=shadowsocks-libev
+PKG_VERSION=2.4.5
+PKG_SOURCE_VERSION=v2.4.5
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://github.com/shadowsocks/$PKG_NAME/archive/$PKG_SOURCE_VERSION.tar.gz"
+PKG_SOURCE_UNTAR_FIXUP=1
+PKG_DEPENDS='libev openssl zlib'
+
+. "$PWD/env.sh"
+
+CONFIGURE_ARGS="$CONFIGURE_ARGS		\\
+	--enable-shared					\\
+	--enable-static					\\
+	--disable-silent-rules			\\
+"
