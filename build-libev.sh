71c876008d5ae3214e78298fb081a13926a53b21
Yousong Zhou <yszhou4tech@gmail.com>
2016-02-04 20:13:04 +0800

    libev: initial version 4.22

---
+++ build-libev.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+PKG_NAME=libev
+PKG_VERSION=4.22
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="http://dist.schmorp.de/libev/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=bf0007e37240103d3c2be80ca9bee3f9
+
+. "$PWD/env.sh"
