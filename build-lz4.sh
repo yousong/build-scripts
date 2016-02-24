c763e86d5c62ef84bfc7496b22c5c5e35cbebd3f
Yousong Zhou <yszhou4tech@gmail.com>
2016-02-24 18:15:22 +0800

    lz4: initial version r131

---
+++ build-lz4.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+PKG_NAME=lz4
+PKG_VERSION=r131
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://github.com/Cyan4973/lz4/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_UNTAR_FIXUP=1
+
+. "$PWD/env.sh"
+
+configure() {
+	true
+}
+
+MAKE_VARS="PREFIX='$INSTALL_PREFIX'"
+# target 'all' includes binaries built with `-m32`
+MAKE_ARGS='lz4programs lib'
