f38ac5935b8a829003c6610da7698667e589ee18
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-02 10:28:16 +0800

    p11-kit: initial version 0.23.2

---
+++ build-p11-kit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+PKG_NAME=p11-kit
+PKG_VERSION=0.23.2
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://p11-glue.freedesktop.org/releases/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=738af2442331fc22f440df9bee9b062a
+
+. "$PWD/env.sh"
