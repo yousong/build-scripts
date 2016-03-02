0ec39006383707df9073d29e5285cf91c0529438
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-02 10:28:01 +0800

    gmp: initial version 6.1.0

---
+++ build-gmp.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+PKG_NAME=gmp
+PKG_VERSION=6.1.0
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SOURCE_URL="https://gmplib.org/download/gmp/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=a9868ef2556ad6a2909babcd1428f3c7
+
+. "$PWD/env.sh"
