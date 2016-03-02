87bde74c8d4b03e680d75a858520af85162dbd0d
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-02 10:28:47 +0800

    libtasn1: initial version 4.7

---
+++ build-libtasn1.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+PKG_NAME=libtasn1
+PKG_VERSION=4.7
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="http://ftp.gnu.org/gnu/libtasn1/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=12d10ca4ae0a3b95f7aa06a076da39ec
+
+. "$PWD/env.sh"
