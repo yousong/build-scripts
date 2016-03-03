35e41b37d4561d94112c110320334e3bf17aed77
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-03 21:08:06 +0800

    sysbench: initial version 0.5

---
+++ build-sysbench.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -e
+
+PKG_NAME=sysbench
+PKG_VERSION=0.5
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://github.com/akopytov/sysbench/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS=lua
+
+. "$PWD/env.sh"
+if os_is_linux; then
+	PKG_DEPENDS="$PKG_DEPENDS libaio"
+fi
+
+configure_pre() {
+	cd "$PKG_SOURCE_DIR"
+	./autogen.sh
+}
+
+# sysbench includes lua source code in sysbench/scripting/lua.  Drizzle and
+# libattachsql support still requires those two libraries first
+#
+#	checking whether to compile with MySQL support... (cached) no
+#	checking whether to compile with Drizzle support... (cached) yes
+#	checking whether to compile with libattachsql support... (cached) yes
+#	checking whether to compile with Oracle support... (cached) no
+#	checking whether to compile with PostgreSQL support... (cached) no
+#
+CONFIGURE_ARGS="$CONFIGURE_ARGS	\\
+	--without-mysql				\\
+"
