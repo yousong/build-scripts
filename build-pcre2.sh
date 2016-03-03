74d54f35394caa56db94bfae08f8e7aa2846fb94
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-03 21:12:48 +0800

    pcre2: initial version 10.21

---
+++ build-pcre2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+PKG_NAME=pcre2
+PKG_VERSION=10.21
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_SOURCE_URL="ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=e79460519f916e3fcb204e59714bfd4a
+
+. "$PWD/env.sh"
