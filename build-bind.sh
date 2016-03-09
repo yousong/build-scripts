4b400f5ad03da74f673ca20ada6284c7fbf02c7d
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-09 12:13:42 +0800

    bind: initial version 9.9.8-P3

---
+++ build-bind.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+PKG_NAME=bind
+PKG_VERSION=9.9.8-P3
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="ftp://ftp.isc.org/isc/bind9/$PKG_VERSION/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=30b9bf88a78eee783d3fef5257445788
+PKG_DEPENDS='openssl zlib'
+
+. "$PWD/env.sh"
