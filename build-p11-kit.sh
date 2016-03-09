458e86ea0f040f91c20d52d69c509796ddf4276f
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-09 12:12:56 +0800

    p11-kit: add dependency on libtasn1 and libffi

--- build-p11-kit.sh
+++ build-p11-kit.sh
@@ -5,5 +5,6 @@ PKG_VERSION=0.23.2
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="https://p11-glue.freedesktop.org/releases/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=738af2442331fc22f440df9bee9b062a
+PKG_DEPENDS='libtasn1 libffi'
 
 . "$PWD/env.sh"
