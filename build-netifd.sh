ce7cb258c59cc0f2ddbaecaeaab0efba30168101
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-10 18:18:02 +0800

    netifd: fix detection of libnl-3.0

--- build-netifd.sh
+++ build-netifd.sh
@@ -12,3 +12,24 @@ PKG_DEPENDS='libnl3 libubox lua5.1 ubus uci'
 PKG_CMAKE=1
 
 . "$PWD/env.sh"
+
+do_patch() {
+	cd "$PKG_SOURCE_DIR"
+	patch -p0 <<"EOF"
+--- CMakeLists.txt.orig	2016-03-10 18:08:38.488381413 +0800
++++ CMakeLists.txt	2016-03-10 18:11:47.088445183 +0800
+@@ -22,8 +22,10 @@ SET(LIBS
+ 	ubox ubus uci json-c blobmsg_json)
+ 
+ IF (NOT DEFINED LIBNL_LIBS)
+-  FIND_LIBRARY(libnl NAMES libnl-3 libnl nl-3 nl)
+-  SET(LIBNL_LIBS ${libnl})
++	INCLUDE(FindPkgConfig)
++	PKG_SEARCH_MODULE(libnl libnl-3.0 libnl-3 nl-3 libnl nl)
++	SET(LIBNL_LIBS ${libnl_LDFLAGS})
++	ADD_DEFINITIONS(${libnl_CFLAGS})
+ ENDIF()
+ 
+ IF("${CMAKE_SYSTEM_NAME}" MATCHES "Linux" AND NOT DUMMY_MODE)
+EOF
+}
