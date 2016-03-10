3868bba71eed9792953f8b7bf85af70296cdf388
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-10 17:39:22 +0800

    ubus: initial version 2016-03-07

---
+++ build-ubus.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+PKG_NAME=ubus
+PKG_VERSION=2016-03-07
+PKG_SOURCE_VERSION=fcf5d8af65f41d6a106ad08d1df5de9729f5399a
+PKG_SOURCE="$PKG_NAME-${PKG_VERSION}-${PKG_SOURCE_VERSION}.tar.gz"
+PKG_SOURCE_URL="http://git.openwrt.org/?p=project/ubus.git;a=snapshot;h=$PKG_SOURCE_VERSION;sf=tgz"
+PKG_SOURCE_UNTAR_FIXUP=1
+PKG_DEPENDS='libubox lua5.1'
+PKG_CMAKE=1
+
+. "$PWD/env.sh"
