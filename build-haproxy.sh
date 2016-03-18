f8be42ba65fcd5f71087cdd57de1429882485b67
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-18 11:56:21 +0800

    haproxy: initial version 1.6.4

---
+++ build-haproxy.sh
@@ -0,0 +1,61 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=haproxy
+PKG_VERSION=1.6.4
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="http://www.haproxy.org/download/${PKG_VERSION%.*}/src/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=ee107312ef58432859ee12bf048025ab
+PKG_DEPENDS='lua5.3 openssl pcre zlib'
+
+. "$PWD/env.sh"
+
+do_patch() {
+	cd "$PKG_SOURCE_DIR"
+	patch -p0 <<"EOF"
+--- Makefile.orig	2016-03-18 11:20:12.921054841 +0800
++++ Makefile	2016-03-18 11:20:33.129060321 +0800
+@@ -705,7 +705,7 @@ endif
+ #### Global link options
+ # These options are added at the end of the "ld" command line. Use LDFLAGS to
+ # add options at the beginning of the "ld" command line if needed.
+-LDOPTS = $(TARGET_LDFLAGS) $(OPTIONS_LDFLAGS) $(ADDLIB)
++LDOPTS = $(TARGET_LDFLAGS) $(OPTIONS_LDFLAGS) $(ADDLIB) $(EXTRA_LDFLAGS)
+ 
+ ifeq ($(TARGET),)
+ all:
+EOF
+}
+
+if os_is_linux; then
+	MAKE_VARS="$MAKE_VARS	\\
+		TARGET=linux2628	\\
+	"
+elif os_is_darwin; then
+	MAKE_VARS="$MAKE_VARS	\\
+		TARGET=osx			\\
+	"
+fi
+MAKE_VARS="$MAKE_VARS		\\
+	USE_PCRE=1				\\
+	USE_PCRE_JIT=1			\\
+	USE_REGPARM=1			\\
+	USE_OPENSSL=1			\\
+	USE_ZLIB=1				\\
+	EXTRA_LDFLAGS='$EXTRA_LDFLAGS' \\
+"
+
+MAKE_VARS="$MAKE_VARS		\\
+	USE_LUA=1				\\
+	LUA_LIB_NAME=lua		\\
+	LUA_INC=$(pkg-config --cflags-only-I lua5.3 | sed -e 's/-I//g')	\\
+	LUA_LD_FLAGS='$(pkg-config --libs lua5.3)'	\\
+"
+
+configure() {
+	true
+}
