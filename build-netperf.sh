e8d6fe5768081060c7bf919aaac7886a6d46bd7a
Yousong Zhou <yszhou4tech@gmail.com>
2016-03-25 15:34:38 +0800

    netperf: initial version 2.7.0

---
+++ build-netperf.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=netperf
+PKG_VERSION=2.7.0
+PKG_SOURCE=netperf-$PKG_VERSION.tar.bz2
+PKG_SOURCE_URL="ftp://ftp.netperf.org/netperf/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=ad1c5342d61f297c417a93133aeba65a
+
+. "$PWD/env.sh"
