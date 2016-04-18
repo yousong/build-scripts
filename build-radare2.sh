5f7e8bf98ba292387b2f892f88ce6ca23f4ee7da
Yousong Zhou <yszhou4tech@gmail.com>
2016-04-18 11:05:44 +0800

    radare2: initial version 0.10.2

---
+++ build-radare2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=radare2
+PKG_VERSION=0.10.2
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SOURCE_URL="http://www.radare.org/get/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=1f70f6735c13c6d34888c452a189ba5b
+
+. "$PWD/env.sh"
