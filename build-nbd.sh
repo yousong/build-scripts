c97c9f6f248973a78f32149ca74fdadbe4925d6d
Yousong Zhou <yszhou4tech@gmail.com>
2016-04-27 13:28:17 +0800

    nbd: initial version 3.13

---
+++ build-nbd.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=nbd
+PKG_VERSION=3.13
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://downloads.sourceforge.net/nbd/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=a48ad35ffdcd156477d29388d92e7489
+PKG_PLATFORM=linux
+
+. "$PWD/env.sh"
