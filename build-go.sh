44f9ea1a632153a8ce069bc733638e4f7c2714ff
Yousong Zhou <yszhou4tech@gmail.com>
2016-06-11 18:18:31 +0800

    go: bump to 1.5.4

--- build-go.sh
+++ build-go.sh
@@ -15,10 +15,10 @@
 # - Go 1.5 Release Notes, https://golang.org/doc/go1.5
 #
 PKG_NAME=go
-PKG_VERSION=1.5.3
+PKG_VERSION=1.5.4
 PKG_SOURCE="$PKG_NAME$PKG_VERSION.src.tar.gz"
 PKG_SOURCE_URL="https://storage.googleapis.com/golang/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=80a0eac7ab750b01b3f7096a1d4667b8
+PKG_SOURCE_MD5SUM=a04d570515c46e4935c63605cbd3a04e
 PKG_DEPENDS=go1.4
 
 . "$PWD/env.sh"
