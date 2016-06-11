037bb5150afe35446a2d0b892796b71195da0d20
Yousong Zhou <yszhou4tech@gmail.com>
2016-06-11 19:52:52 +0800

    libunwind: initial version 1.1

---
+++ build-libunwind.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=libunwind
+PKG_VERSION=1.1
+PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.gz"
+PKG_SOURCE_URL="http://download.savannah.gnu.org/releases/libunwind/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=fb4ea2f6fbbe45bf032cd36e586883ce
+
+. "$PWD/env.sh"
