806aa52c26d8a23d729bd948caa8363f9ce970ad
Yousong Zhou <yszhou4tech@gmail.com>
2016-07-01 16:34:38 +0800

    ag: update depends to include pcre and zlib

--- build-ag.sh
+++ build-ag.sh
@@ -18,13 +18,12 @@
 #
 # See https://github.com/ggreer/the_silver_searcher for details.
 #
-
 PKG_NAME=ag
 PKG_VERSION=0.31.0
 PKG_SOURCE="the_silver_searcher-${PKG_VERSION}.tar.gz"
 PKG_SOURCE_URL="http://geoff.greer.fm/ag/releases/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=6a4275e0191e7fe2f834f7ec00eabbbe
 PKG_SOURCE_UNTAR_FIXUP=1
-PKG_DEPENDS=xz
+PKG_DEPENDS='pcre xz zlib'
 
 . "$PWD/env.sh"
