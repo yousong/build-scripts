1799c9b963563878164428181ecd61ff08a2beec
Yousong Zhou <yszhou4tech@gmail.com>
2016-07-04 14:48:41 +0800

    socat: use version 2.0.0-b8
    The SOCKS5 client and address chain feature is useful

--- build-socat.sh
+++ build-socat.sh
@@ -8,10 +8,10 @@
 # socat on Debian Wheezy 7 has version 1.7.1.3 and lacking readline support
 #
 PKG_NAME=socat
-PKG_VERSION=1.7.3.0
+PKG_VERSION=2.0.0-b8
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_URL="http://www.dest-unreach.org/socat/download/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM="b607edb65bc6c57f4a43f06247504274"
+PKG_SOURCE_MD5SUM=ff56576703dfdeac221357a348c30760
 PKG_DEPENDS='readline openssl'
 
 . "$PWD/env.sh"
