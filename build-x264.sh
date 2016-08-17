5ec4b3065052d60a4db315e00bd6c0a2a48c25a6
Yousong Zhou <yszhou4tech@gmail.com>
2016-08-17 15:05:24 +0800

    x264: initial version 20160816-2245

---
+++ build-x264.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=x264
+PKG_VERSION=20160816-2245
+PKG_SOURCE="$PKG_NAME-snapshot-${PKG_VERSION}.tar.bz2"
+PKG_SOURCE_URL="http://download.videolan.org/pub/videolan/x264/snapshots/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=3bce82e73eff372942d5a7972951f7f4
+
+. "$PWD/env.sh"
+
+CONFIGURE_ARGS="$CONFIGURE_ARGS		\\
+	--enable-shared					\\
+"
