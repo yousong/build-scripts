939557cb4ff97309da9fb607f58856e6b5a31192
Yousong Zhou <yszhou4tech@gmail.com>
2016-08-17 15:05:57 +0800

    ffmpeg: enable libx264, libx265, libfdk-aac, libmp3lame

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -5,16 +5,23 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
+# - Compilation Guide, https://trac.ffmpeg.org/wiki/CompilationGuide
+#
 PKG_NAME=ffmpeg
 PKG_VERSION=3.1.1
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_URL="http://ffmpeg.org/releases/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=bee939350e80e07c3d63285d0873b66b
-PKG_DEPENDS='bzip2 zlib'
+PKG_DEPENDS='bzip2 fdk-aac x264 x265 yasm zlib'
 
 . "$PWD/env.sh"
 
 CONFIGURE_ARGS="$CONFIGURE_ARGS		\\
 	--enable-shared					\\
-	--disable-yasm					\\
+	--enable-gpl					\\
+	--enable-nonfree				\\
+	--enable-libfdk-aac				\\
+	--enable-libx264				\\
+	--enable-libx265				\\
+	--enable-libmp3lame				\\
 "
