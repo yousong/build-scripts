fe0dad4bdaa675d3ec76a05c4d38ee6240e2e83a
Yousong Zhou <yszhou4tech@gmail.com>
2016-08-17 15:21:33 +0800

    lame: initial version 3.99.5

---
+++ build-lame.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=lame
+PKG_VERSION=3.99.5
+PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.gz"
+PKG_SOURCE_URL="https://downloads.sourceforge.net/$PKG_NAME/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=84835b313d4a8b68f5349816d33e07ce
+PKG_DEPENDS=nasm
+
+. "$PWD/env.sh"
+
+CONFIGURE_ARGS="$CONFIGURE_ARGS		\\
+	--enable-nasm					\\
+"
