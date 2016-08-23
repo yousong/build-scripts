4a5f776d2ffed4ed0787fc1e009f5e261e6a3746
Yousong Zhou <yszhou4tech@gmail.com>
2016-08-23 11:31:06 +0800

    libpng: initial version 1.6.24

---
+++ build-libpng.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=libpng
+PKG_VERSION=1.6.24
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SOURCE_URL="https://downloads.sourceforge.net/libpng/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=ffcdbd549814787fa8010c372e35ff25
+
+. "$PWD/env.sh"
