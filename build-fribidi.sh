8efb321df3146d95573f6f54f8b03c3fb446ac45
Yousong Zhou <yszhou4tech@gmail.com>
2016-08-23 10:39:34 +0800

    fribidi: initial version 0.19.7

---
+++ build-fribidi.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=fribidi
+PKG_VERSION=0.19.7
+PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.bz2"
+PKG_SOURCE_URL="http://fribidi.org/download/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=6c7e7cfdd39c908f7ac619351c1c5c23
+
+. "$PWD/env.sh"
