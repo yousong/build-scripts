f78cdcb676a5e303cf8caf40f2e67819f344f37d
Yousong Zhou <yszhou4tech@gmail.com>
2016-08-25 16:56:25 +0800

    libjpeg-turbo: initial version 1.5.0

---
+++ build-libjpeg-turbo.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=libjpeg-turbo
+PKG_VERSION=1.5.0
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://downloads.sourceforge.net/libjpeg-turbo/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=3fc5d9b6a8bce96161659ae7a9939257
+
+. "$PWD/env.sh"
+
+CONFIGURE_ARGS="$CONFIGURE_ARGS	\\
+	--disable-silent-rules		\\
+"
