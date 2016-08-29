07be6d0bd39f522ca8d1d1a0f9181b332cdc959d
Yousong Zhou <yszhou4tech@gmail.com>
2016-08-29 11:17:37 +0800

    libunwind: set platform of libunwind to linux

--- build-libunwind.sh
+++ build-libunwind.sh
@@ -10,5 +10,6 @@ PKG_VERSION=1.1
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.gz"
 PKG_SOURCE_URL="http://download.savannah.gnu.org/releases/libunwind/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=fb4ea2f6fbbe45bf032cd36e586883ce
+PKG_PLATFORM=linux
 
 . "$PWD/env.sh"
