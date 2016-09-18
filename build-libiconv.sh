b682fd0b2b7f5098d212a774b90e07deda92d76b
Yousong Zhou <yszhou4tech@gmail.com>
2016-09-18 17:38:34 +0800

    libiconv: remove empty PKG_DEPENDS

--- build-libiconv.sh
+++ build-libiconv.sh
@@ -10,6 +10,5 @@ PKG_VERSION=1.14
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://ftp.gnu.org/pub/gnu/libiconv/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=e34509b1623cec449dfeb73d7ce9c6c6
-PKG_DEPENDS=''
 
 . "$PWD/env.sh"
