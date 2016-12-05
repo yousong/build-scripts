8e44fc84b5113cf70298e33ae49cb0a457bdfcc8
Yousong Zhou <yszhou4tech@gmail.com>
2016-12-05 15:13:57 +0800

    libcap-ng: initial version 0.7.8

---
+++ build-libcap-ng.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+#
+# Copyright 2016 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=libcap-ng
+PKG_VERSION=0.7.8
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://people.redhat.com/sgrubb/libcap-ng/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=0dece96644bd798020e170fbf7663802
+
+. "$PWD/env.sh"
