5f41fc410c6395b2b0a8fa8e06d47e899fc54c3a
Yousong Zhou <yszhou4tech@gmail.com>
2017-02-28 15:46:54 +0800

    openvpn: bump to version 2.4.0

--- build-openvpn.sh
+++ build-openvpn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 #
-# Copyright 2015-2016 (c) Yousong Zhou
+# Copyright 2015-2017 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -16,10 +16,10 @@
 # PKCS11 requires libpkcs11-helper-1 >= 1.11, which is not available in Debian Wheezy (only 1.09 is available)
 #
 PKG_NAME=openvpn
-PKG_VERSION=2.3.8
+PKG_VERSION=2.4.0
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.xz"
 PKG_SOURCE_URL="https://swupdate.openvpn.org/community/releases/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=acc5ea4b08ad53173784520acbd4e9c3
+PKG_SOURCE_MD5SUM=e49431542f065dd60a0181233927669e
 PKG_DEPENDS='openssl lzo'
 
 . "$PWD/env.sh"
