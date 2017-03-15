1236b63e012f753d99aafcd3a588167d66746d26
Yousong Zhou <yszhou4tech@gmail.com>
2017-03-15 11:07:21 +0800

    go: bump to version 1.7.5

--- build-go.sh
+++ build-go.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 #
-# Copyright 2015-2016 (c) Yousong Zhou
+# Copyright 2015-2017 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -14,11 +14,15 @@
 # - Release History, https://golang.org/doc/devel/release.html
 # - Go 1.5 Release Notes, https://golang.org/doc/go1.5
 #
+# Additional tools
+#
+#	go get golang.org/x/tour/gotour
+#
 PKG_NAME=go
-PKG_VERSION=1.7.4
+PKG_VERSION=1.7.5
 PKG_SOURCE="$PKG_NAME$PKG_VERSION.src.tar.gz"
 PKG_SOURCE_URL="https://storage.googleapis.com/golang/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=49c1076428a5d3b5ad7ac65233fcca2f
+PKG_SOURCE_MD5SUM=506de2d870409e9003e1440bcfeb3a65
 PKG_DEPENDS=go1.4
 
 . "$PWD/env.sh"
