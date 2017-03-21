c6bf96782dd6c19c1a7912ae199b5892d1407e85
Yousong Zhou <yszhou4tech@gmail.com>
2017-03-21 21:27:07 +0800

    zlib: bump to version 1.2.11
    As the url to the old version now returns 404 NOTFOUND

--- build-zlib.sh
+++ build-zlib.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2017 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=zlib
-PKG_VERSION=1.2.8
+PKG_VERSION=1.2.11
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_SOURCE_URL="http://zlib.net/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=28f1205d8dd2001f26fec1e8c2cebe37
+PKG_SOURCE_MD5SUM=85adef240c5f370b308da8c938951a68
 
 . "$PWD/env.sh"
