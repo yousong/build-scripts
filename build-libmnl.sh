3cd90e0415c31beeed6784ae513b11725ea6f730
Yousong Zhou <yszhou4tech@gmail.com>
2017-05-07 00:24:01 +0800

    libmnl: initial version 1.0.4

---
+++ build-libmnl.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+#
+# Copyright 2017 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=libmnl
+PKG_VERSION=1.0.4
+PKG_SOURCE="libmnl-$PKG_VERSION.tar.bz2"
+PKG_SOURCE_URL="https://www.netfilter.org/projects/libmnl/files/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=be9b4b5328c6da1bda565ac5dffadb2d
+PKG_PLATFORM=linux
+
+. "$PWD/env.sh"
