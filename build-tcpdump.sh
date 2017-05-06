3876ad7c830d04e9085ad7ab5425f8e2cce9af37
Yousong Zhou <yszhou4tech@gmail.com>
2017-05-06 21:07:20 +0800

    tcpdump: bump to version 4.9.0

--- build-tcpdump.sh
+++ build-tcpdump.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2017 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=tcpdump
-PKG_VERSION=4.7.4
+PKG_VERSION=4.9.0
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://www.tcpdump.org/release/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=58af728de36f499341918fc4b8e827c3
+PKG_SOURCE_MD5SUM=2b83364eef53b63ca3181b4eb56dab0c
 PKG_DEPENDS='libpcap openssl'
 
 . "$PWD/env.sh"
