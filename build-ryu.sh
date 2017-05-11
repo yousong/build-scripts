afc09e1fe9a7a78b101853feff7392991512bfe9
Yousong Zhou <yszhou4tech@gmail.com>
2017-05-11 15:01:30 +0800

    ryu: add notes in install_post()

--- build-ryu.sh
+++ build-ryu.sh
@@ -5,16 +5,6 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
-# It requries several extra python packages to install or for additional
-# features
-#
-#	pip install -r tools/pip-requires
-#	pip install -r tools/optional-requires
-#
-# Examples
-#
-#	ryu-manager --verbose ryu.app.simple_switch_13
-# 
 PKG_NAME=ryu
 PKG_VERSION=4.13
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
@@ -25,3 +15,19 @@ PKG_DEPENDS='python2'
 
 . "$PWD/env.sh"
 . "$PWD/utils-python.sh"
+
+install_post() {
+	cat <<EOF
+Ryu requries several extra python packages to run and provide additional
+features
+
+	cd "$PKG_SOURCE_DIR"
+	pip install -r tools/pip-requires
+	pip install -r tools/optional-requires
+
+Examples
+
+	ryu-manager --verbose ryu.app.simple_switch_13
+
+EOF
+}
