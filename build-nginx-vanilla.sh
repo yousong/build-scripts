39e17e60446d96d4fb00c5704905c62c293da000
Yousong Zhou <yszhou4tech@gmail.com>
2017-05-15 13:17:51 +0800

    nginx: bump to version 1.13.0
    NGINX 1.12.0 is based on 1.11.13 and is the first release of the new
    stable branch 1.12.  NGINX 1.13.0 the first dev release of the master
    branch
    
    - https://www.nginx.com/blog/nginx-1-12-1-13-released

--- build-nginx-vanilla.sh
+++ build-nginx-vanilla.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 #
-# Copyright 2015-2016 (c) Yousong Zhou
+# Copyright 2015-2017 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -26,10 +26,10 @@
 # - Benchmark results across accept_mutex, reuseport, https://www.nginx.com/blog/socket-sharding-nginx-release-1-9-1/
 #
 PKG_NAME=nginx
-PKG_VERSION=1.11.4
+PKG_VERSION=1.13.0
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://nginx.org/download/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=92666d60410f429ddbdb10060369a480
+PKG_SOURCE_MD5SUM=0ff7057465d5622c5b1279e91b273e96
 PKG_DEPENDS='openssl pcre zlib'
 
 . "$PWD/env.sh"
