54ce1eb0db3e5234a766585097c6834619214bca
Yousong Zhou <yszhou4tech@gmail.com>
2017-05-26 16:30:51 +0800

    ansible: initial version 2.3.0.0

---
+++ build-ansible.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+#
+# Copyright 2017 (c) Yousong Zhou
+#
+# Install requirements
+#
+#	pip install -r requirements.txt
+#
+# Example
+#
+#	# github streisand
+#	ansible-playbook playbooks/streisand.yml -vvv
+#
+PKG_NAME=ansible
+PKG_VERSION=2.3.0.0
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="http://releases.ansible.com/ansible/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=cf19fa58f534700322250e70bb24fcc9
+PKG_PYTHON_VERSIONS=2
+
+. "$PWD/env.sh"
+. "$PWD/utils-python.sh"
