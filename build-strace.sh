93f4eb245577b149bf6287f7150a940439e91fbc
Yousong Zhou <yszhou4tech@gmail.com>
2017-06-08 15:03:02 +0800

    strace: bump to version 4.17 to fix configure script bug

--- build-strace.sh
+++ build-strace.sh
@@ -6,14 +6,19 @@
 # See /LICENSE for more information.
 #
 PKG_NAME=strace
-PKG_VERSION=4.12
+PKG_VERSION=4.17
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.xz"
 PKG_SOURCE_URL="https://downloads.sourceforge.net/strace/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=efb8611fc332e71ec419c53f59faa93e
+PKG_SOURCE_MD5SUM=8d7eb10eba68bad83a269197e634b626
 PKG_DEPENDS=libunwind
 
 . "$PWD/env.sh"
 
+# 4.12 has a bug fixed by "7c0e887": configure.ac: fix checks for btrfs
+# specific structures
+#
+# we cannot use PKG_AUTOCONF_FIXUP because the AC_INIT refers to a utility
+# called ./git-version-gen which is not packaged in the relesae tarball
 CONFIGURE_ARGS="$CONFIGURE_ARGS	\\
 	--with-libunwind=yes		\\
 "
