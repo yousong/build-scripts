49ce0493275569810a0606b8d6e3ab432ac3057c
Yousong Zhou <yszhou4tech@gmail.com>
2017-06-08 10:14:42 +0800

    fio: fix compilation

--- build-fio.sh
+++ build-fio.sh
@@ -18,6 +18,34 @@ if os_is_linux; then
 	PKG_DEPENDS="$PKG_DEPENDS libaio"
 fi
 
+do_patch() {
+	cd "$PKG_SOURCE_DIR"
+
+	patch -p0 <<"EOF"
+Fixes compiling error
+
+	In file included from oslib/libmtd_legacy.c:38:0:
+	oslib/libmtd.h:288:8: error: unknown type name ‘uint8_t’
+	oslib/libmtd.h:305:4: error: unknown type name ‘uint64_t’
+	oslib/libmtd.h:305:20: error: unknown type name ‘uint64_t’
+	oslib/libmtd.h:322:5: error: unknown type name ‘uint64_t’
+	oslib/libmtd.h:322:21: error: unknown type name ‘uint64_t’
+	make[2]: *** [oslib/libmtd_legacy.o] Error 1
+
+--- oslib/libmtd.h.orig	2017-06-07 22:34:54.164956014 +0800
++++ oslib/libmtd.h	2017-06-07 22:35:04.848959358 +0800
+@@ -29,6 +29,8 @@
+ extern "C" {
+ #endif
+ 
++#include <stdint.h>
++
+ /* Maximum MTD device name length */
+ #define MTD_NAME_MAX 127
+ /* Maximum MTD device type string length */
+EOF
+}
+
 CONFIGURE_ARGS="$CONFIGURE_ARGS							\\
 	--extra-cflags='$EXTRA_CFLAGS $EXTRA_LDFLAGS'		\\
 "
