1ff26234d48506b6d0eb5a00252e74f01e86e117
Yousong Zhou <yszhou4tech@gmail.com>
2017-06-11 15:35:30 +0800

    x264: depends on yasm

--- build-x264.sh
+++ build-x264.sh
@@ -10,6 +10,7 @@ PKG_VERSION=20160816-2245
 PKG_SOURCE="$PKG_NAME-snapshot-${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_URL="http://download.videolan.org/pub/videolan/x264/snapshots/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=3bce82e73eff372942d5a7972951f7f4
+PKG_DEPENDS="yasm"
 
 . "$PWD/env.sh"
 
