72142a585d79578cf6a4bfef4bac699a531fce5b
Yousong Zhou <yszhou4tech@gmail.com>
2017-07-17 18:58:44 +0800

    tengine: add naxsi

--- build-nginx-tengine.sh
+++ build-nginx-tengine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2015-2016 (c) Yousong Zhou
+# Copyright 2015-2017 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -20,3 +20,26 @@ CONFIGURE_ARGS+=(
 	--with-http_mp4_module
 	--with-http_v2_module
 )
+
+# naxsi
+#
+#	http {
+#		include /path/to/naxsi_config/naxsi_core.rules;
+#		server {
+#			set $naxsi_extensive_log 1;
+#			location /foo {
+#				SecRulesEnabled;
+#				#LearningMode;
+#				CheckRule "$SQL >= 8" BLOCK;
+#				CheckRule "$CSS >= 8" BLOCK;
+#				DeniedUrl "/50x.html";
+#			}
+#		}
+#	}
+#
+# naxsi wiki: https://github.com/nbs-system/naxsi/wiki/naxsi-setup
+#
+MODS='
+	0.55.3:nbs-system/naxsi:naxsi_src
+'
+nginx_add_modules
