30712ad2e2cddabd08ed6f135ac74b8aab030a57
Yousong Zhou <yszhou4tech@gmail.com>
2017-08-16 11:19:59 +0800

    riscvemu: bump to version 2017-08-06
    Now it has x86 kvm support, sdl support, etc.

--- build-riscvemu.sh
+++ build-riscvemu.sh
@@ -6,10 +6,10 @@
 # See /LICENSE for more information.
 #
 PKG_NAME=riscvemu
-PKG_VERSION=2017-01-12
+PKG_VERSION=2017-08-06
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.gz"
 PKG_SOURCE_URL="http://bellard.org/riscvemu/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=94e1173749d42e65370c6a44bc51ddf8
+PKG_SOURCE_MD5SUM=5d8ffc2d9966b900794c9cbb477ff16a
 PKG_DEPENDS='curl'
 
 . "$PWD/env.sh"
@@ -18,18 +18,58 @@ do_patch() {
 	cd "$PKG_SOURCE_DIR"
 
 	patch -p0 <<"EOF"
---- Makefile.orig	2017-01-10 11:40:43.589357310 +0800
-+++ Makefile	2017-01-10 11:41:31.517372310 +0800
-@@ -30,6 +30,9 @@ RISCVEMU_OBJS+=fs_net.o
- RISCVEMU_LIBS+=-lcurl
+--- Makefile.orig	2017-08-16 10:50:08.192673537 +0800
++++ Makefile	2017-08-16 10:50:59.780689683 +0800
+@@ -98,6 +98,9 @@ LDFLAGS+=-mwindows
+ endif
  endif
  
 +CFLAGS+=$(EXTRA_CFLAGS)
 +LDFLAGS+=$(EXTRA_LDFLAGS)
 +
- riscvemu32: riscvemu32.o $(RISCVEMU_OBJS)
- 	$(CC) $(LDFLAGS) -o $@ $^ $(RISCVEMU_LIBS)
+ RISCVEMU_OBJS:=$(EMU_OBJS) riscvemu.o riscv_machine.o softfp.o
  
+ X86EMU_OBJS:=$(EMU_OBJS) x86emu.o x86_cpu.o x86_machine.o ide.o ps2.o vmmouse.o pckbd.o vga.o
+EOF
+	patch -p0 <<"EOF"
+Fix the following compilation errors
+
+	riscvemu.c: In function ‘virt_machine_run’:
+	riscvemu.c:584:31: error: ‘stdin_fd’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
+	gcc -O2 -Wall -g -Werror -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -MMD -D_GNU_SOURCE -DCONFIG_VERSION=\"2017-08-06\" -DCONFIG
+	_SLIRP -DCONFIG_FS_NET -DCONFIG_SDL -isystem /home/yousong/.usr/include -c -o ps2.o ps2.c
+	cc1: all warnings being treated as errors
+	make: *** [riscvemu.o] Error 1
+--- riscvemu.c.orig	2017-08-16 10:54:03.644747231 +0800
++++ riscvemu.c	2017-08-16 10:55:07.344767168 +0800
+@@ -541,9 +541,6 @@ void virt_machine_run(VirtMachine *m)
+     fd_set rfds, wfds, efds;
+     int fd_max, ret, delay;
+     struct timeval tv;
+-#ifndef _WIN32
+-    int stdin_fd;
+-#endif
+     
+     delay = virt_machine_get_sleep_duration(m, MAX_SLEEP_TIME);
+     
+@@ -555,7 +552,7 @@ void virt_machine_run(VirtMachine *m)
+ #ifndef _WIN32
+     if (m->console_dev && virtio_console_can_write_data(m->console_dev)) {
+         STDIODevice *s = m->console->opaque;
+-        stdin_fd = s->stdin_fd;
++        int stdin_fd = s->stdin_fd;
+         FD_SET(stdin_fd, &rfds);
+         fd_max = stdin_fd;
+ 
+@@ -581,6 +578,8 @@ void virt_machine_run(VirtMachine *m)
+     }
+     if (ret > 0) {
+ #ifndef _WIN32
++        STDIODevice *s = m->console->opaque;
++        int stdin_fd = s->stdin_fd;
+         if (m->console_dev && FD_ISSET(stdin_fd, &rfds)) {
+             uint8_t buf[128];
+             int ret, len;
 EOF
 }
 
@@ -45,7 +85,10 @@ install() {
 	true
 }
 
+# disable SDL because the version installed by apt-get requires libuuid 1.0
+# (symbol uuid_generate@UUID_1.0) while the one installed by us is libuuid 1.2
 MAKE_VARS+=(
 	EXTRA_CFLAGS="${EXTRA_CFLAGS[*]}"
 	EXTRA_LDFLAGS="${EXTRA_LDFLAGS[*]}"
+	CONFIG_SDL=
 )
