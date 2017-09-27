f7a10abee71dcf779241f7acaf77b4bc56cede71
Yousong Zhou <yszhou4tech@gmail.com>
2017-09-27 10:52:21 +0800

    rsync: remove trailing whitespace

--- build-rsync.sh
+++ build-rsync.sh
@@ -14,6 +14,6 @@ PKG_VERSION=3.1.2
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="https://download.samba.org/pub/rsync/src/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=0f758d7e000c0f7f7d3792610fad70cb
-PKG_DEPENDS="popt libiconv" 
+PKG_DEPENDS="popt libiconv"
 
 . "$PWD/env.sh"
