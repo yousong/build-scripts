655cd6e2624cc0b3af65d914c9599e461ff3b00e
Yousong Zhou <yszhou4tech@gmail.com>
2017-10-17 05:07:06 -0400

    libiconv: bump to version 1.15
    - Fix build failure caused by gets() being dropped in c11,
      https://lists.gnu.org/archive/html/bug-gnulib/2012-03/msg00186.html

--- build-libiconv.sh
+++ build-libiconv.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2017 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=libiconv
-PKG_VERSION=1.14
+PKG_VERSION=1.15
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://ftp.gnu.org/pub/gnu/libiconv/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=e34509b1623cec449dfeb73d7ce9c6c6
+PKG_SOURCE_MD5SUM=ace8b5f2db42f7b3b3057585e80d9808
 
 . "$PWD/env.sh"
