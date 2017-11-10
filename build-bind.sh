53da6445f7ea19274e88b31f49d113074bc854ac
Yousong Zhou <yszhou4tech@gmail.com>
2017-11-10 14:55:37 +0800

    bind: bump to version 9.10.6

--- build-bind.sh
+++ build-bind.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2017 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=bind
-PKG_VERSION=9.9.8-P3
+PKG_VERSION=9.10.6
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="ftp://ftp.isc.org/isc/bind9/$PKG_VERSION/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=30b9bf88a78eee783d3fef5257445788
+PKG_SOURCE_MD5SUM=84e663284b17aee0df1ce6f248b137d7
 PKG_DEPENDS='openssl zlib'
 
 . "$PWD/env.sh"
