1659093476a304462384ca65e58392c80b87450f
Yousong Zhou <yszhou4tech@gmail.com>
2018-05-11 11:00:08 +0800

    llvm: add lld

--- build-llvm.sh
+++ build-llvm.sh
@@ -55,6 +55,46 @@ PKG_libcxxabi_SOURCE="libcxxabi-$PKG_libcxxabi_VERSION.src.tar.xz"
 PKG_libcxxabi_SOURCE_URL="http://llvm.org/releases/$PKG_libcxxabi_VERSION/$PKG_libcxxabi_SOURCE"
 PKG_libcxxabi_SOURCE_MD5SUM=9d06327892fc5d8acec4ef2e2821ab3d
 
+# A linker, https://lld.llvm.org/
+#
+# To use it
+#
+#	./clang \
+#		--sysroot=$HOME/git-repo/lede-project/lede/staging_dir/toolchain-mips_24kc_gcc-7.3.0_glibc \
+#		-target mips-openwrt-linux-gnu \
+#		-mfloat-abi=soft \
+#		-fuse-ld=lld \
+#		-Wl,-dynamic-linker -Wl,/lib/ld-musl-mips-sf.so.1 \
+#		-v \
+#		-o a a.c
+#
+#	./clang \
+#		--sysroot=$HOME/git-repo/lede-project/lede/staging_dir/toolchain-mips_24kc_gcc-7.3.0_musl \
+#		-target mips-openwrt-linux-musl \
+#		-mfloat-abi=soft \
+#		-fuse-ld=lld \
+#		-Wl,-dynamic-linker -Wl,/lib/ld-musl-mips-sf.so.1 \
+#		-v \
+#		-o a a.c
+#
+# LLVM is born cross-capable ;) Linker will need -sysroot to find "crt*.o" and
+# "libgcc*" under "$sysroot/lib/gcc/$target".  The simple test above have the
+# following result
+#
+#  - glibc: compiles and run with OpenWrt malta musl machine
+#  - musl: compiles but segfault when running in OpenWrt malta musl machine
+#
+#		root@OpenWrt:/tmp# /tmp/a
+#		[  677.202865] do_page_fault(): sending SIGSEGV to a for invalid write access to 00020000
+#		[  677.207014] epc = 77f6e8f0 in libc.so[77eeb000+92000]
+#		[  677.210336] ra  = 77f6e3b8 in libc.so[77eeb000+92000]
+#
+PKG_lld_NAME=lld
+PKG_lld_VERSION=$LLVM_VERSION
+PKG_lld_SOURCE="lld-$PKG_lld_VERSION.src.tar.xz"
+PKG_lld_SOURCE_URL="http://llvm.org/releases/$PKG_lld_VERSION/$PKG_lld_SOURCE"
+PKG_lld_SOURCE_MD5SUM=7ab2612417477b03538f11cd8b5e12f8
+
 # test-suite too big: about 113MB as of version 3.9.1
 PKG_test_suite_NAME=test-suite
 PKG_test_suite_VERSION=$LLVM_VERSION
@@ -66,6 +106,7 @@ PKG_test_suite_SOURCE_MD5SUM=x
 
 download_extra() {
 	download_http "$PKG_clang_SOURCE"		"$PKG_clang_SOURCE_URL"			"$PKG_clang_SOURCE_MD5SUM"
+	download_http "$PKG_lld_SOURCE"			"$PKG_lld_SOURCE_URL"			"$PKG_lld_SOURCE_MD5SUM"
 	download_http "$PKG_compiler_rt_SOURCE"	"$PKG_compiler_rt_SOURCE_URL"	"$PKG_compiler_rt_SOURCE_MD5SUM"
 	download_http "$PKG_libcxx_SOURCE"		"$PKG_libcxx_SOURCE_URL"		"$PKG_libcxx_SOURCE_MD5SUM"
 	download_http "$PKG_libcxxabi_SOURCE"	"$PKG_libcxxabi_SOURCE_URL"		"$PKG_libcxxabi_SOURCE_MD5SUM"
@@ -73,6 +114,7 @@ download_extra() {
 
 prepare_extra() {
 	unpack "$BASE_DL_DIR/$PKG_clang_SOURCE"			"$PKG_SOURCE_DIR/tools/"	"s:^[^/]\\+:clang:"
+	unpack "$BASE_DL_DIR/$PKG_lld_SOURCE"			"$PKG_SOURCE_DIR/tools/"	"s:^[^/]\\+:lld:"
 	unpack "$BASE_DL_DIR/$PKG_compiler_rt_SOURCE"	"$PKG_SOURCE_DIR/projects/"	"s:^[^/]\\+:compiler-rt:"
 	unpack "$BASE_DL_DIR/$PKG_libcxx_SOURCE"		"$PKG_SOURCE_DIR/projects/"	"s:^[^/]\\+:libcxx:"
 	unpack "$BASE_DL_DIR/$PKG_libcxxabi_SOURCE"		"$PKG_SOURCE_DIR/projects/"	"s:^[^/]\\+:libcxxabi:"
