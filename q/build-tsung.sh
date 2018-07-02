6e030238fcc581bdbf85b6ad1647fbdbedc87263
Yousong Zhou <yszhou4tech@gmail.com>
2018-07-02 16:06:38 +0800

    q: add bcc and tsung

---
+++ q/build-tsung.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+#
+# Copyright 2017 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=tsung
+PKG_VERSION=1.6.0
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="http://tsung.erlang-projects.org/dist/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=ecefc861c2e0c1f99da459261559055a
+
+. "$PWD/env.sh"
