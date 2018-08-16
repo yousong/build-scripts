e66cbca1887e0028061d9b5fa7a5aeb5ea08dbdf
Yousong Zhou <zhouyousong@yunionyun.com>
2018-08-16 06:59:56 +0000

    haproxy: bump to version 1.8.13

--- build-haproxy.sh
+++ build-haproxy.sh
@@ -1,43 +1,18 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2018 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=haproxy
-PKG_VERSION=1.6.4
+PKG_VERSION=1.8.13
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://www.haproxy.org/download/${PKG_VERSION%.*}/src/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=ee107312ef58432859ee12bf048025ab
+PKG_SOURCE_MD5SUM=bf0b437bad78f5824d7e26ae0c81fee4
 PKG_DEPENDS='lua5.3 openssl pcre zlib'
 
 . "$PWD/env.sh"
-
-do_patch() {
-	cd "$PKG_SOURCE_DIR"
-	patch -p0 <<"EOF"
---- Makefile.orig	2016-03-18 11:20:12.921054841 +0800
-+++ Makefile	2016-03-18 11:20:33.129060321 +0800
-@@ -300,7 +300,6 @@ ifeq ($(TARGET),osx)
-   USE_POLL       = implicit
-   USE_KQUEUE     = implicit
-   USE_TPROXY     = implicit
--  USE_LIBCRYPT   = implicit
- else
- ifeq ($(TARGET),openbsd)
-   # This is for OpenBSD >= 3.0
-@@ -705,7 +705,7 @@ endif
- #### Global link options
- # These options are added at the end of the "ld" command line. Use LDFLAGS to
- # add options at the beginning of the "ld" command line if needed.
--LDOPTS = $(TARGET_LDFLAGS) $(OPTIONS_LDFLAGS) $(ADDLIB)
-+LDOPTS = $(TARGET_LDFLAGS) $(OPTIONS_LDFLAGS) $(ADDLIB) $(EXTRA_LDFLAGS)
- 
- ifeq ($(TARGET),)
- all:
-EOF
-}
 
 if os_is_linux; then
 	MAKE_VARS+=(
@@ -50,7 +25,7 @@ elif os_is_darwin; then
 fi
 MAKE_VARS+=(
 	PREFIX="$INSTALL_PREFIX"
-	EXTRA_LDFLAGS="${EXTRA_LDFLAGS[*]}"
+	LDFLAGS="${EXTRA_LDFLAGS[*]}"
 	USE_PCRE=1
 	USE_PCRE_JIT=1
 	USE_REGPARM=1
