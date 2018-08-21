ab7aa70583da7bac52deb34f87f3537d4b924a7f
Yousong Zhou <zhouyousong@yunionyun.com>
2018-08-21 15:02:38 +0000

    lua5.3: bump to version 5.3.5

--- build-lua5.3.sh
+++ build-lua5.3.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2018 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=lua5.3
-PKG_VERSION=5.3.2
+PKG_VERSION=5.3.5
 PKG_SOURCE="lua-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://www.lua.org/ftp/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=33278c2ab5ee3c1a875be8d55c1ca2a1
+PKG_SOURCE_MD5SUM=4f4b4f323fd3514a68e0ab3da8ce3455
 
 . "$PWD/env.sh"
 . "$PWD/utils-lua.sh"
