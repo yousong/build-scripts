a3a4504674ad63a311dde2c7e866a650c1a866da
Yousong Zhou <zhouyousong@yunionyun.com>
2018-08-21 00:44:08 +0000

    openvswitch: bump to version 2.10.0

--- build-openvswitch.sh
+++ build-openvswitch.sh
@@ -35,10 +35,10 @@
 #     /usr/local/share/openvswitch/scripts/ovs-ctl force-reload-kmod --system-id=random
 #
 PKG_NAME=openvswitch
-PKG_VERSION=2.9.2
+PKG_VERSION=2.10.0
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://openvswitch.org/releases/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=11f6ffd0ec958dd59adad402ae596478
+PKG_SOURCE_MD5SUM=33a55c9bac1fcaa8842f84a175e50800
 PKG_DEPENDS=openssl
 PKG_PLATFORM=linux
 
