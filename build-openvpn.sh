d0f3fb2cb1b1f42f0e0d91484869f03f31325759
Yousong Zhou <zhouyousong@yunionyun.com>
2018-08-28 02:21:17 +0000

    openvpn: bump to version 2.4.6

--- build-openvpn.sh
+++ build-openvpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2015-2017 (c) Yousong Zhou
+# Copyright 2015-2018 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -16,10 +16,14 @@
 # PKCS11 requires libpkcs11-helper-1 >= 1.11, which is not available in Debian Wheezy (only 1.09 is available)
 #
 PKG_NAME=openvpn
-PKG_VERSION=2.4.0
+PKG_VERSION=2.4.6
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.xz"
 PKG_SOURCE_URL="https://swupdate.openvpn.org/community/releases/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=e49431542f065dd60a0181233927669e
+PKG_SOURCE_MD5SUM=3a1f3f63bdaede443b4df49957df9405
 PKG_DEPENDS='openssl lzo'
 
 . "$PWD/env.sh"
+
+CONFIGURE_ARGS+=(
+	--disable-plugin-auth-pam
+)
