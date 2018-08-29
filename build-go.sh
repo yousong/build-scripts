784f286410d7c348aa45f16fd84e1687399c5fab
Yousong Zhou <zhouyousong@yunionyun.com>
2018-08-29 12:03:42 +0000

    go: bump to version 1.10.4

--- build-go.sh
+++ build-go.sh
@@ -20,10 +20,10 @@
 #	go get golang.org/x/tools/cmd/godoc
 #
 PKG_NAME=go
-PKG_VERSION=1.10.3
+PKG_VERSION=1.10.4
 PKG_SOURCE="$PKG_NAME$PKG_VERSION.src.tar.gz"
 PKG_SOURCE_URL="https://storage.googleapis.com/golang/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=d15dfb264105c5e84fbe33f4a4aa5021
+PKG_SOURCE_MD5SUM=8094c8adc70ea92bc96ecfaf1d570e79
 PKG_DEPENDS=go1.4
 
 . "$PWD/env.sh"
