841c13ec5b9835a1488f482beac08d1eb76a83b9
Yousong Zhou <zhouyousong@yunionyun.com>
2018-09-03 07:19:22 +0000

    pcl: initial version 1.12

---
+++ build-pcl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+#
+# Copyright 2018 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=pcl
+PKG_VERSION=1.12
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="http://www.xmailserver.org/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=816d394743d8ab1b3e20af1efebb093d
+
+. "$PWD/env.sh"
