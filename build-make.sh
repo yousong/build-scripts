d794225f8612edd1aa52df7af21e751b3156a739
Yousong Zhou <zhouyousong@yunionyun.com>
2018-09-09 02:39:05 +0000

    make: initial version 4.2.1
    GNU glibc 2.28 requries gmake of version at least 4.0

---
+++ build-make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+#
+# Copyright 2018 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+PKG_NAME=make
+PKG_VERSION=4.2.1
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_SOURCE_URL="https://ftp.gnu.org/gnu/make/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=15b012617e7c44c0ed482721629577ac
+PKG_DEPENDS=''
+
+. "$PWD/env.sh"
