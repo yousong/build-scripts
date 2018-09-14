16b6be0e13b64db356558f62d020dfd6c10c7b54
Yousong Zhou <zhouyousong@yunionyun.com>
2018-09-14 04:25:18 +0000

    keepalived: bump to version 2.0.7

--- build-keepalived.sh
+++ build-keepalived.sh
@@ -1,46 +1,25 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2018 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=keepalived
-PKG_VERSION=1.2.19
+PKG_VERSION=2.0.7
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://www.keepalived.org/software/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=5c98b06639dd50a6bff76901b53febb6
-PKG_DEPENDS='libnl3 net-snmp openssl'
+PKG_SOURCE_MD5SUM=5204f541c75f4f68339809f0761693c5
+PKG_DEPENDS='libnl3 openssl'
 PKG_PLATFORM=linux
 
 . "$PWD/env.sh"
-
-do_patch() {
-	cd "$PKG_SOURCE_DIR"
-
-	patch -p0 <<"EOF"
---- Makefile.in.orig	2016-01-14 17:58:09.101827556 +0800
-+++ Makefile.in	2016-01-14 17:58:42.385837973 +0800
-@@ -63,9 +63,9 @@ install:
- 	$(MAKE) -C keepalived install
- 	$(MAKE) -C genhash install
- ifeq (@SNMP_SUPPORT@, _WITH_SNMP_)
--	mkdir -p $(DESTDIR)/usr/share/snmp/mibs/
--	cp -f doc/VRRP-MIB $(DESTDIR)/usr/share/snmp/mibs/
--	cp -f doc/KEEPALIVED-MIB $(DESTDIR)/usr/share/snmp/mibs/
-+	mkdir -p $(DESTDIR)@prefix@/share/snmp/mibs/
-+	cp -f doc/VRRP-MIB $(DESTDIR)@prefix@/share/snmp/mibs/
-+	cp -f doc/KEEPALIVED-MIB $(DESTDIR)@prefix@/share/snmp/mibs/
- endif
- 
- tarball: mrproper
-EOF
-}
 
 # clear up -I$INSTALL_PREFIX/include to avoid libipvs to include
 # netlink/netlink.h from libnl1
 EXTRA_CFLAGS=()
 CONFIGURE_ARGS+=(
-	--enable-snmp
+	--disable-snmp
 	--enable-sha1
+	--without-init	# do not install any types of initscripts
 )
