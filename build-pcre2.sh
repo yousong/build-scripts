7d053ce1e5209025f063ac62eaa9c1e34286d4f4
Yousong Zhou <zhouyousong@yunionyun.com>
2018-09-25 09:37:58 +0000

    pcre2: enable unicode and jit

--- build-pcre2.sh
+++ build-pcre2.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2018 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
+#
+# TODO
+#
+#  - https://rust-leipzig.github.io/regex/2017/03/28/comparison-of-regex-engines/
 #
 PKG_NAME=pcre2
 PKG_VERSION=10.21
@@ -12,3 +16,8 @@ PKG_SOURCE_URL="ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/$PKG_SOURC
 PKG_SOURCE_MD5SUM=e79460519f916e3fcb204e59714bfd4a
 
 . "$PWD/env.sh"
+
+CONFIGURE_ARGS+=(
+	--enable-unicode
+	--enable-jit
+)
