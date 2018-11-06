2b041613bbcb540a57933438222f16af9ea0fcd0
Yousong Zhou <zhouyousong@yunionyun.com>
2018-11-06 13:10:30 +0000

    libev: remove compat include/event.h
    Quote commit desc from openwrt/packages
    
    	commit 0b66804f251709b2321a7d1fcd09c645008ac28c
    	Author: Ted Hess <thess@kitschensync.net>
    	Date:   Fri Dec 11 17:58:41 2015 -0500
    
    	    libev: Fix compatibility with libevent2 includes
    
    	    Packages building with libev and want to include event.h from libev
    	    need to be modified to include ev_event_compat.h instead.
    
    	    The are no known packages which need to be fixed at this time. The
    	    normal include is ev.h for this package.
    
    	    Signed-off-by: Ted Hess <thess@kitschensync.net>

--- build-libev.sh
+++ build-libev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2018 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -12,3 +12,9 @@ PKG_SOURCE_URL="http://dist.schmorp.de/libev/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=94459a5a22db041dec6f98424d6efe54
 
 . "$PWD/env.sh"
+
+staging_post() {
+	# the compat header can override libevent2's
+	rm -vf "$PKG_STAGING_DIR$INSTALL_PREFIX/include/event.h"
+	staging_post_default
+}
