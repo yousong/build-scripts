36dfa05805083dbae8a1f4acfd74cf51ef53353a
Yousong Zhou <zhouyousong@yunionyun.com>
2018-12-10 03:48:27 +0000

    wrk: bump to version 4.1.0
    wrk-4.1.0 has luajit-2.1.0-beta3 source code packaged which is currently
    a development version

--- build-wrk.sh
+++ build-wrk.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2018 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=wrk
-PKG_VERSION=2015-11-04
-PKG_SOURCE_VERSION=03dc368674402f4b26a862f941f29887d06fd564
-PKG_SOURCE="$PKG_NAME-$PKG_VERSION-$PKG_SOURCE_VERSION.tar.gz"
-PKG_SOURCE_URL="https://github.com/wg/wrk/archive/$PKG_SOURCE_VERSION.tar.gz"
+PKG_VERSION=4.1.0
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://github.com/wg/wrk/archive/$PKG_VERSION.tar.gz"
 PKG_SOURCE_UNTAR_FIXUP=1
 PKG_DEPENDS='openssl'
 
@@ -21,34 +20,17 @@ do_patch() {
 	patch -p0 <<"EOF"
 --- Makefile.orig	2016-01-04 01:30:30.640813279 +0800
 +++ Makefile	2016-01-04 01:34:22.872885305 +0800
-@@ -1,4 +1,4 @@
--CFLAGS  := -std=c99 -Wall -O2 -D_REENTRANT
-+CFLAGS  += -std=c99 -Wall -O2 -D_REENTRANT
- LIBS    := -lpthread -lm -lcrypto -lssl
+@@ -102,6 +102,10 @@ $(LDIR)/libluajit.a:
  
- TARGET  := $(shell uname -s | tr '[A-Z]' '[a-z]' 2>/dev/null || echo unknown)
-@@ -26,8 +26,8 @@ $(LDIR)/libluajit.a:
- 
- LDIR     = deps/luajit/src
- LIBS    := -lluajit $(LIBS)
--CFLAGS  += -I$(LDIR)
--LDFLAGS += -L$(LDIR)
-+CFLAGS  := -I$(LDIR) $(CFLAGS)
-+LDFLAGS := -L$(LDIR) $(LDFLAGS)
- 
- all: $(BIN)
- 
-@@ -56,6 +56,10 @@ $(LDIR)/libluajit.a:
- 	@echo Building LuaJIT...
- 	@$(MAKE) -C $(LDIR) BUILDMODE=static
+ # ------------
  
 +install:
 +	mkdir -p $(DESTDIR)$(PREFIX)/bin
 +	install -m 0755 wrk $(DESTDIR)$(PREFIX)/bin
 +
  .PHONY: all clean
- .SUFFIXES:
- .SUFFIXES: .c .o .lua
+ .PHONY: $(ODIR)/version.o
+ 
 EOF
 }
 
@@ -56,6 +38,8 @@ configure() {
 	true
 }
 
+#	WITH_LUAJIT="$INSTALL_PREFIX"
 MAKE_VARS+=(
 	PREFIX="$INSTALL_PREFIX"
+	WITH_OPENSSL="$INSTALL_PREFIX"
 )
