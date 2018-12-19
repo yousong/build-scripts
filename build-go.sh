531fa13889912616d84a513fffe7e56f189b76ed
Yousong Zhou <zhouyousong@yunionyun.com>
2018-12-19 07:41:47 +0000

    go: bump to version 1.11.4
    go1.11.4 happened 2 days after go1.11.3
    
    Release notes from https://golang.org/doc/devel/release.html#go1.11
    
      go1.11.3 (released 2018/12/12) includes three security fixes to "go get" and
      the crypto/x509 package. See the Go 1.11.3 milestone on our issue tracker for
      details.
    
      go1.11.4 (released 2018/12/14) includes fixes to cgo, the compiler, linker,
      runtime, documentation, go command, and the net/http and go/types packages. It
      includes a fix to a bug introduced in Go 1.11.3 that broke go get for import
      path patterns containing "...". See the Go 1.11.4 milestone on our issue
      tracker for details.

--- build-go.sh
+++ build-go.sh
@@ -20,10 +20,10 @@
 #	go get golang.org/x/tools/cmd/godoc
 #
 PKG_NAME=go
-PKG_VERSION=1.11.2
+PKG_VERSION=1.11.4
 PKG_SOURCE="$PKG_NAME$PKG_VERSION.src.tar.gz"
 PKG_SOURCE_URL="https://storage.googleapis.com/golang/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=0b808590b1b10f1072b5f52105d8e285
+PKG_SOURCE_MD5SUM=a77697673215be465d1b583680ef2318
 PKG_DEPENDS=go1.4
 
 . "$PWD/env.sh"
