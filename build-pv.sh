ce63148d8850dbb34d98a2638c4ebfdb2b35ab39
Yousong Zhou <zhouyousong@yunionyun.com>
2018-12-29 13:46:52 +0000

    pv: initial version 1.6.6

---
+++ build-pv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+#
+# Copyright 2018 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=pv
+PKG_VERSION=1.6.6
+PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.bz2"
+PKG_SOURCE_URL="http://www.ivarch.com/programs/sources/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=ff3564fddcc2b9bd4a9c1d143aba4b4c
+
+. "$PWD/env.sh"
