5ddb34423906704d0f19737ccd547c6568339f69
Yousong Zhou <zhouyousong@yunionyun.com>
2019-01-08 15:29:05 +0000

    haproxy: initial version 1.9.1

--- build-haproxy.sh
+++ build-haproxy.sh
@@ -14,10 +14,10 @@
 # TODO use libslz
 #
 PKG_NAME=haproxy
-PKG_VERSION=1.9.0
+PKG_VERSION=1.9.1
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://www.haproxy.org/download/${PKG_VERSION%.*}/src/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=be67ae289d9261940a31e5bc9a3bf79a
+PKG_SOURCE_MD5SUM=7694f210115e880af4e9e62cd357aebd
 PKG_DEPENDS='lua5.3 openssl pcre zlib'
 
 . "$PWD/env.sh"
