8d31071a1e45a68419f0e88daf60ba279d6b4b41
Yousong Zhou <zhouyousong@yunionyun.com>
2019-02-18 02:54:45 +0000

    pixman: bump to version 0.38.0

--- build-pixman.sh
+++ build-pixman.sh
@@ -6,10 +6,10 @@
 # See /LICENSE for more information.
 #
 PKG_NAME=pixman
-PKG_VERSION=0.34.0
+PKG_VERSION=0.38.0
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="https://www.cairographics.org/releases/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=e80ebae4da01e77f68744319f01d52a3
+PKG_SOURCE_MD5SUM=8f34a92041de2daaa4c34c5f7c860f21
 PKG_DEPENDS=''
 
 . "$PWD/env.sh"
