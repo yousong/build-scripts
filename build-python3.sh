8188731afcef0373f3b961784acb50e99de8271c
Yousong Zhou <zhouyousong@yunionyun.com>
2019-02-28 14:00:04 +0000

    python3: bump to version 3.7.2

--- build-python3.sh
+++ build-python3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2015-2018 (c) Yousong Zhou
+# Copyright 2015-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -15,10 +15,10 @@
 #	it. https://gcc.gnu.org/bugzilla/show_bug.cgi?id=46158
 #
 PKG_NAME=python3
-PKG_VERSION=3.7.1
+PKG_VERSION=3.7.2
 PKG_SOURCE="Python-${PKG_VERSION}.tar.xz"
 PKG_SOURCE_URL="https://www.python.org/ftp/python/$PKG_VERSION/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=0a57e9022c07fad3dadb2eef58568edb
+PKG_SOURCE_MD5SUM=df6ec36011808205beda239c72f947cb
 PKG_DEPENDS='bzip2 db openssl ncurses readline sqlite zlib'
 
 . "$PWD/env.sh"
