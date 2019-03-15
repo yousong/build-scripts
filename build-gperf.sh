091b20c7b382e356c32c0557d4f83a31417d0ea8
Yousong Zhou <zhouyousong@yunionyun.com>
2019-03-15 08:37:03 +0000

    gperf: initial version 3.1

---
+++ build-gperf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+#
+# Copyright 2019 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=gperf
+PKG_VERSION=3.1
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://ftp.gnu.org/gnu/$PKG_NAME/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=9e251c0a618ad0824b51117d5d9db87e
+
+. "$PWD/env.sh"
