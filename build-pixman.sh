84cef58aea01d0c1ed5ea2f91408214e1d028077
Yousong Zhou <zhouyousong@yunionyun.com>
2019-04-15 01:55:07 +0000

    pixman: bump to version 0.38.4

--- build-pixman.sh
+++ build-pixman.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2018 (c) Yousong Zhou
+# Copyright 2018-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=pixman
-PKG_VERSION=0.38.0
+PKG_VERSION=0.38.4
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="https://www.cairographics.org/releases/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=8f34a92041de2daaa4c34c5f7c860f21
+PKG_SOURCE_MD5SUM=267a7af290f93f643a1bc74490d9fdd1
 PKG_DEPENDS=''
 
 . "$PWD/env.sh"
