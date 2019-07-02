76ad65d647ae2c95c103fead4d1db260619ccc61
Yousong Zhou <zhouyousong@yunionyun.com>
2019-07-02 08:36:17 +0000

    mitmproxy: bump to version 4.0.4
    Requirement for glibc version is too high for centos7, see [1]
    
     [1] https://github.com/mitmproxy/mitmproxy/issues/3533

--- build-mitmproxy.sh
+++ build-mitmproxy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2018 (c) Yousong Zhou
+# Copyright 2018-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=mitmproxy
-PKG_VERSION=3.0.3
+PKG_VERSION=4.0.4
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION-linux.tar.gz"
-PKG_SOURCE_URL="https://github.com/mitmproxy/mitmproxy/releases/download/v$PKG_VERSION/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=2c11cf153c4f708e0a91892124e0acc1
+PKG_SOURCE_URL="https://snapshots.mitmproxy.org/$PKG_VERSION/mitmproxy-$PKG_VERSION-linux.tar.gz"
+PKG_SOURCE_MD5SUM=bee8543567ab572c50eb7188af8aad66
 PKG_DEPENDS=linux		# it also has binaries for osx and windows
 
 . "$PWD/env.sh"
