f7692fa69e5d05f58413360e18f8f5b224e97f34
Yousong Zhou <zhouyousong@yunionyun.com>
2019-07-16 02:19:08 +0000

    curl: enable c-ares

--- build-curl.sh
+++ build-curl.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 #
-# Copyright 2015-2016 (c) Yousong Zhou
+# Copyright 2015-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
+# @c-ares for --dns-xx options support
 # @nghttp2 for HTTP2 support
 # @rtmpdump for RTMP support
 # @openssl for SSL support
@@ -14,6 +15,10 @@ PKG_VERSION=7.46.0
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_SOURCE_URL="http://curl.haxx.se/download/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=9979f989a2a9930d10f1b3deeabc2148
-PKG_DEPENDS='nghttp2 rtmpdump openssl zlib'
+PKG_DEPENDS='c-ares nghttp2 rtmpdump openssl zlib'
 
 . "$PWD/env.sh"
+
+CONFIGURE_ARGS+=(
+	--enable-ares
+)
