89a8624247976839d4e322bf12d8eb2824e92442
Yousong Zhou <zhouyousong@yunionyun.com>
2019-07-18 11:34:47 +0000

    iproute2: bump to version 5.2.0

--- build-iproute2.sh
+++ build-iproute2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2017 (c) Yousong Zhou
+# Copyright 2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=iproute2
-PKG_VERSION=4.11.0
+PKG_VERSION=5.2.0
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.xz"
 PKG_SOURCE_URL="https://www.kernel.org/pub/linux/utils/net/iproute2/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=7a9498de88bcca95c305df6108ae197e
+PKG_SOURCE_MD5SUM=0cb2736e7bc2f56254a363d3d23703b7
 PKG_DEPENDS='db elfutils libmnl'
 PKG_PLATFORM=linux
 
@@ -20,31 +20,29 @@ do_patch() {
 
 	# - fail fast on installation failure
 	patch -p1 <<"EOF"
-diff --git a/Makefile b/Makefile
-index 18de7dcb..761f7ba6 100644
---- a/Makefile
-+++ b/Makefile
-@@ -30,6 +30,7 @@ DEFINES+= -DNO_SHARED_LIBS
+--- a/Makefile.orig	2019-07-18 11:31:12.827598663 +0000
++++ b/Makefile	2019-07-18 11:31:42.270537771 +0000
+@@ -37,6 +37,7 @@ DEFINES+= -DNO_SHARED_LIBS
  endif
  
- DEFINES+=-DCONFDIR=\"$(CONFDIR)\"
-+DEFINES+=-DARPDDIR=\"$(ARPDDIR)\"
+ DEFINES+=-DCONFDIR=\"$(CONFDIR)\" \
++	 -DARPDDIR=\"$(ARPDDIR)\" \
+          -DNETNS_RUN_DIR=\"$(NETNS_RUN_DIR)\" \
+          -DNETNS_ETC_DIR=\"$(NETNS_ETC_DIR)\"
  
- #options for decnet
- ADDLIB+=dnet_ntop.o dnet_pton.o
-@@ -81,7 +82,7 @@ install: all
+@@ -92,7 +93,7 @@ install: all
  		$(DESTDIR)$(DOCDIR)/examples
  	install -m 0644 $(shell find examples/diffserv -maxdepth 1 -type f) \
  		$(DESTDIR)$(DOCDIR)/examples/diffserv
--	@for i in $(SUBDIRS) doc; do $(MAKE) -C $$i install; done
-+	@set -e; for i in $(SUBDIRS) doc; do $(MAKE) -C $$i install; done
+-	@for i in $(SUBDIRS);  do $(MAKE) -C $$i install; done
++	@set -e; for i in $(SUBDIRS);  do $(MAKE) -C $$i install; done
  	install -m 0644 $(shell find etc/iproute2 -maxdepth 1 -type f) $(DESTDIR)$(CONFDIR)
  	install -m 0755 -d $(DESTDIR)$(BASH_COMPDIR)
  	install -m 0644 bash-completion/tc $(DESTDIR)$(BASH_COMPDIR)
 --- a/misc/arpd.c.orig	2017-06-11 09:02:38.769871128 +0800
 +++ b/misc/arpd.c	2017-06-11 09:02:43.077872476 +0800
-@@ -41,7 +41,7 @@
- int resolve_hosts;
+@@ -39,7 +39,7 @@
+ #include "rt_names.h"
  
  DB	*dbase;
 -char	*dbname = "/var/lib/arpd/arpd.db";
