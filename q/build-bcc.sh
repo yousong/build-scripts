b2c609338450e19d2c71ee1b56f5dec43544d003
Yousong Zhou <zhouyousong@yunionyun.com>
2019-07-25 07:09:49 +0000

    bcc: bump to version 0.10.0

--- q/build-bcc.sh
+++ q/build-bcc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 #
-# Copyright 2017 (c) Yousong Zhou
+# Copyright 2017-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=bcc
-PKG_VERSION=0.3.0
+PKG_VERSION=0.10.0
 PKG_SOURCE_VERSION="$PKG_VERSION"
-PKG_SOURCE="$PKG_NAME-$PKG_VERSION-$PKG_SOURCE_VERSION.tar.gz"
+PKG_SOURCE="$PKG_NAME-$PKG_SOURCE_VERSION.tar.gz"
 PKG_SOURCE_URL="https://github.com/iovisor/bcc/archive/v$PKG_SOURCE_VERSION.tar.gz"
 PKG_DEPENDS='luajit libelf'
 PKG_SOURCE_UNTAR_FIXUP=1
@@ -17,26 +17,21 @@ PKG_CMAKE=1
 . "$PWD/env.sh"
 env_init_llvm_toolchain
 
-do_patch() {
-	cd "$PKG_SOURCE_DIR"
+libbpf_SOURCE_URL="https://github.com/libbpf/libbpf.git"
+libbpf_VERSION=59a6415
+libbpf_SOURCE=libbpf-2019-05-24.tar.gz
 
-	patch -p1 <<"EOF"
---- a/src/python/CMakeLists.txt.orig	2017-06-24 19:08:10.912790987 +0800
-+++ b/src/python/CMakeLists.txt	2017-06-24 19:08:15.940792560 +0800
-@@ -21,9 +21,6 @@ add_custom_command(OUTPUT ${PIP_INSTALLA
-   )
- add_custom_target(bcc_py ALL DEPENDS ${PIP_INSTALLABLE})
- 
--if(EXISTS "/etc/debian_version")
--  set(PYTHON_FLAGS "${PYTHON_FLAGS} --install-layout deb")
--endif()
- install(CODE "execute_process(COMMAND ${PYTHON_CMD} setup.py install -f ${PYTHON_FLAGS}
--  --prefix=${CMAKE_INSTALL_PREFIX} WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})"
-+  --root=$(DESTDIR) --prefix=${CMAKE_INSTALL_PREFIX} WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})"
-   COMPONENT python)
-EOF
+download_extra() {
+	download_git libbpf "$libbpf_SOURCE_URL" "$libbpf_VERSION" "$libbpf_SOURCE"
 }
 
+prepare_extra() {
+	unpack "$BASE_DL_DIR/$libbpf_SOURCE" "$PKG_SOURCE_DIR/src/cc" "s:^[^/]\\+:libbpf:"
+}
+
+EXTRA_CXXFLAGS+=(
+	-D_GLIBCXX_USE_CXX11_ABI=1
+)
 CMAKE_ARGS+=(
-	-DLLVM_DIR="$INSTALL_PREFIX/toolchain/llvm-4.0.0/lib/cmake/llvm"
+	-DLLVM_DIR="$LLVM_DIR/lib/cmake/llvm"
 )
