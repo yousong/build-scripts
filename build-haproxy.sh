e9c088268a854400e4c68fbb5b2781f74e961e4c
Yousong Zhou <zhouyousong@yunionyun.com>
2019-08-13 10:35:10 +0000

    haproxy: bump to version 2.0.4

--- build-haproxy.sh
+++ build-haproxy.sh
@@ -14,10 +14,10 @@
 # TODO use libslz
 #
 PKG_NAME=haproxy
-PKG_VERSION=2.0.1
+PKG_VERSION=2.0.4
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://www.haproxy.org/download/${PKG_VERSION%.*}/src/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=96dd4897ac46d704e18554541686238d
+PKG_SOURCE_MD5SUM=e0a295b3aa468f70ba261cc5ae9a5f83
 PKG_DEPENDS='lua5.3 openssl pcre zlib'
 
 . "$PWD/env.sh"
