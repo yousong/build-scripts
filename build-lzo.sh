740ae92b85478c19300b8663538410a7c99b64d4
Yousong Zhou <zhouyousong@yunionyun.com>
2019-08-19 08:37:55 +0000

    lzo: bump to version 2.10

--- build-lzo.sh
+++ build-lzo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=lzo
-PKG_VERSION=2.09
+PKG_VERSION=2.10
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://www.oberhumer.com/opensource/lzo/download/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=c7ffc9a103afe2d1bba0b015e7aa887f
+PKG_SOURCE_MD5SUM=39d3f3f9c55c87b1e5d6888e1420f4b5
 
 . "$PWD/env.sh"
 
