25e1808faeebc6f01277f22c75389a15c08efbc2
Yousong Zhou <zhouyousong@yunionyun.com>
2019-10-25 08:12:42 +0000

    ubus: bump to version 2019-07-01

--- build-ubus.sh
+++ build-ubus.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=ubus
-PKG_VERSION=2016-03-07
-PKG_SOURCE_VERSION=fcf5d8af65f41d6a106ad08d1df5de9729f5399a
+PKG_VERSION=2019-07-01
+PKG_SOURCE_VERSION=2e051f62899666805d477830ef790e1149bc6a89
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}-${PKG_SOURCE_VERSION}.tar.gz"
 PKG_SOURCE_URL="http://git.openwrt.org/?p=project/ubus.git;a=snapshot;h=$PKG_SOURCE_VERSION;sf=tgz"
 PKG_SOURCE_UNTAR_FIXUP=1
