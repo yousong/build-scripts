eed7b11485603b967049d4a01dd2723cb318e032
Yousong Zhou <zhouyousong@yunionyun.com>
2019-10-25 08:20:49 +0000

    uci: bump to version 2019-09-01

--- build-uci.sh
+++ build-uci.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=uci
-PKG_VERSION=2016-03-07
-PKG_SOURCE_VERSION=18c13247f9e0bfad1effc0445bcda436d03789c5
+PKG_VERSION=2019-09-01
+PKG_SOURCE_VERSION=415f9e48436d29f612348f58f546b3ad8d74ac38
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}-${PKG_SOURCE_VERSION}.tar.gz"
 PKG_SOURCE_URL="http://git.openwrt.org/?p=project/uci.git;a=snapshot;h=$PKG_SOURCE_VERSION;sf=tgz"
 PKG_SOURCE_UNTAR_FIXUP=1
