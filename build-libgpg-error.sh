706f46d8f876dda54dbfb003ac337c5559bfff65
Yousong Zhou <zhouyousong@yunionyun.com>
2019-10-25 07:22:09 +0000

    libgpg-error: initial version 1.36

---
+++ build-libgpg-error.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+#
+# Copyright 2019 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=libgpg-error
+PKG_VERSION=1.36
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_SOURCE_URL="https://www.gnupg.org/ftp/gcrypt/libgpg-error/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=eff437f397e858a9127b76c0d87fa5ed
+
+. "$PWD/env.sh"
