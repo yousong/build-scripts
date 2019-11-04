48eb4d172da12fa4f98cc0e755ee900a8be7a2e5
Yousong Zhou <zhouyousong@yunionyun.com>
2019-11-04 03:37:53 +0000

    spice: bump to version 0.14.2

--- build-spice.sh
+++ build-spice.sh
@@ -8,10 +8,10 @@
 # > headers defining protocols
 #
 PKG_NAME=spice
-PKG_VERSION=0.14.1
+PKG_VERSION=0.14.2
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_URL="https://www.spice-space.org/download/releases/spice-server/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=1e2442b08605a5a17430d1aaa0912c24
+PKG_SOURCE_MD5SUM=7ca431580b662e7c3cb8021983a3d387
 PKG_DEPENDS='libjpeg-turbo openssl pixman spice-protocol zlib'
 
 . "$PWD/env.sh"
