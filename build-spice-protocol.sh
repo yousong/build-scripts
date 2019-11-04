88877f3568b4f6206a9b232df6fae9c642d07441
Yousong Zhou <zhouyousong@yunionyun.com>
2019-11-04 03:37:41 +0000

    spice-protocol: bump to version 0.14.0

--- build-spice-protocol.sh
+++ build-spice-protocol.sh
@@ -8,9 +8,9 @@
 # > headers defining protocols
 #
 PKG_NAME=spice-protocol
-PKG_VERSION=0.12.15
+PKG_VERSION=0.14.0
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_URL="https://www.spice-space.org/download/releases/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=e1db63e3ff0cb1f1c98277283356dc51
+PKG_SOURCE_MD5SUM=773a6b31df105a6b3c470eba201bed34
 
 . "$PWD/env.sh"
