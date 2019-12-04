70ce35f498047813525372a5dc89f44c2c7e2c62
Yousong Zhou <zhouyousong@yunionyun.com>
2019-12-04 02:12:13 +0000

    bird: bump to version 2.0.7

--- build-bird.sh
+++ build-bird.sh
@@ -8,9 +8,9 @@
 # configure requires gawk
 #
 PKG_NAME=bird
-PKG_VERSION=2.0.6
+PKG_VERSION=2.0.7
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="ftp://bird.network.cz/pub/bird/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=bc05f225f4fcb365da1dbb308a7e23cc
+PKG_SOURCE_MD5SUM=dc884bbe5905578e452f28158700527c
 
 . "$PWD/env.sh"
