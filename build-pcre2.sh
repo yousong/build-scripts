91aabcdb16fbc01d4155753654e8a986ed9f84f3
Yousong Zhou <yszhou4tech@gmail.com>
2019-12-19 11:39:52 +0800

    pcre2: bump to version 10.34

--- build-pcre2.sh
+++ build-pcre2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2016-2018 (c) Yousong Zhou
+# Copyright 2016-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -10,10 +10,10 @@
 #  - https://rust-leipzig.github.io/regex/2017/03/28/comparison-of-regex-engines/
 #
 PKG_NAME=pcre2
-PKG_VERSION=10.21
+PKG_VERSION=10.34
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_SOURCE_URL="ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=e79460519f916e3fcb204e59714bfd4a
+PKG_SOURCE_URL="https://ftp.pcre.org/pub/pcre/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=d280b62ded13f9ccf2fac16ee5286366
 
 . "$PWD/env.sh"
 
