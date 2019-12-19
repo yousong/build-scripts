054fdc6c44677271677f3727639ccdae552d2d6e
Yousong Zhou <yszhou4tech@gmail.com>
2019-12-19 11:38:27 +0800

    pcre: bump to version 8.43

--- build-pcre.sh
+++ build-pcre.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=pcre
-PKG_VERSION=8.38
+PKG_VERSION=8.43
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_SOURCE_URL="ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=00aabbfe56d5a48b270f999b508c5ad2
+PKG_SOURCE_URL="https://ftp.pcre.org/pub/pcre/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=636222e79e392c3d95dcc545f24f98c4
 
 . "$PWD/env.sh"
 
