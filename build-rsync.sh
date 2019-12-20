4a2d4eb20d1472c7c932d36783eabede1d8c3d7b
Yousong Zhou <yszhou4tech@gmail.com>
2019-12-20 14:34:15 +0800

    rsync: bump to version 3.1.3

--- build-rsync.sh
+++ build-rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2017 (c) Yousong Zhou
+# Copyright 2017-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -26,10 +26,10 @@
 # "reverse lookup" is only available since version 3.1.0
 #
 PKG_NAME=rsync
-PKG_VERSION=3.1.2
+PKG_VERSION=3.1.3
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="https://download.samba.org/pub/rsync/src/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=0f758d7e000c0f7f7d3792610fad70cb
+PKG_SOURCE_MD5SUM=1581a588fde9d89f6bc6201e8129afaf
 PKG_DEPENDS="popt libiconv"
 
 . "$PWD/env.sh"
