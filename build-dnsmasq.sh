0c25d2c82628d6f34e9b49ad30c64f5c40f8cd6b
Yousong Zhou <yszhou4tech@gmail.com>
2019-12-20 11:28:11 +0800

    dnsmasq: bump to version 2.80

--- build-dnsmasq.sh
+++ build-dnsmasq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2016-2017 (c) Yousong Zhou
+# Copyright 2016-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -8,10 +8,10 @@
 # @libnettle is required for DNSSEC support
 #
 PKG_NAME=dnsmasq
-PKG_VERSION=2.78
+PKG_VERSION=2.80
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_SOURCE_URL="http://www.thekelleys.org.uk/dnsmasq/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=6d0241b72c79d2b510776ccc4ed69ca4
+PKG_SOURCE_MD5SUM=e040e72e6f377a784493c36f9e788502
 
 . "$PWD/env.sh"
 
