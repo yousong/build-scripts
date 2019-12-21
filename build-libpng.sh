16a13853cc1b0f16b96deb5cff5db08acb332bcd
Yousong Zhou <yszhou4tech@gmail.com>
2019-12-21 12:56:14 +0800

    libpng: bump to version 1.6.37
    The 1.6.24 version could not be found now (HTTP 404)

--- build-libpng.sh
+++ build-libpng.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=libpng
-PKG_VERSION=1.6.24
+PKG_VERSION=1.6.37
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_SOURCE_URL="https://downloads.sourceforge.net/libpng/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=ffcdbd549814787fa8010c372e35ff25
+PKG_SOURCE_MD5SUM=015e8e15db1eecde5f2eb9eb5b6e59e9
 
 . "$PWD/env.sh"
