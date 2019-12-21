636f08a318ac1fa504954e0d86517c115d576f90
Yousong Zhou <yszhou4tech@gmail.com>
2019-12-21 12:56:44 +0800

    p11-kit: bump to version 0.23.18.1
    Download link moved to github release page

--- build-p11-kit.sh
+++ build-p11-kit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2019 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=p11-kit
-PKG_VERSION=0.23.2
+PKG_VERSION=0.23.18.1
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_SOURCE_URL="https://p11-glue.freedesktop.org/releases/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=738af2442331fc22f440df9bee9b062a
+PKG_SOURCE_URL="https://github.com/p11-glue/p11-kit/releases/download/$PKG_VERSION/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=79480c3a2c905a74f86e885966148537
 PKG_DEPENDS='libtasn1 libffi'
 
 . "$PWD/env.sh"
