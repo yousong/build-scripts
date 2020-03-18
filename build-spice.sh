d49b889d3d86040ebdb29b2b102b733e2710310d
Yousong Zhou <yszhou4tech@gmail.com>
2020-03-18 10:24:49 +0800

    spice: bump to version 0.14.3

--- build-spice.sh
+++ build-spice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2019 (c) Yousong Zhou
+# Copyright 2019-2020 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -8,10 +8,10 @@
 # > headers defining protocols
 #
 PKG_NAME=spice
-PKG_VERSION=0.14.2
+PKG_VERSION=0.14.3
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_URL="https://www.spice-space.org/download/releases/spice-server/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=7ca431580b662e7c3cb8021983a3d387
+PKG_SOURCE_MD5SUM=a776650f7c4dc22681d76308475a9190
 PKG_DEPENDS='libjpeg-turbo openssl pixman spice-protocol zlib'
 
 . "$PWD/env.sh"
