c16df640c89dd59b8019f52af3fffb68afcaf932
Yousong Zhou <yszhou4tech@gmail.com>
2020-03-18 10:24:25 +0800

    dtc: bump to version 1.6.0

--- build-dtc.sh
+++ build-dtc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2019 (c) Yousong Zhou
+# Copyright 2019-2020 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=dtc
-PKG_VERSION=1.5.1
+PKG_VERSION=1.6.0
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_SOURCE_URL="https://mirrors.edge.kernel.org/pub/software/utils/$PKG_NAME/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=d5b67727ee6d168fd83023e995565341
+PKG_SOURCE_MD5SUM=1556ba93648bf70d7aa034252e278751
 
 . "$PWD/env.sh"
 
