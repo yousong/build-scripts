bd0401929d6192d35230f4e473030ec7f9d9a4fd
Yousong Zhou <yszhou4tech@gmail.com>
2020-03-18 10:24:35 +0800

    spice-protocol: bump to version 0.14.1

--- build-spice-protocol.sh
+++ build-spice-protocol.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2019 (c) Yousong Zhou
+# Copyright 2019-2020 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -8,9 +8,9 @@
 # > headers defining protocols
 #
 PKG_NAME=spice-protocol
-PKG_VERSION=0.14.0
+PKG_VERSION=0.14.1
 PKG_SOURCE="$PKG_NAME-${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_URL="https://www.spice-space.org/download/releases/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=773a6b31df105a6b3c470eba201bed34
+PKG_SOURCE_MD5SUM=97fea9d82a8c88a1ba9b6ad0f73860a9
 
 . "$PWD/env.sh"
