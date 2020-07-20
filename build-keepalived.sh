120071faf16166e97d3c07fd48960f2d22493d2a
Yousong Zhou <yszhou4tech@gmail.com>
2020-07-20 11:00:40 +0800

    keepalied: bump to version 2.0.20
    "Handle interface MAC addresses" since
    [2.0.13](https://github.com/acassen/keepalived/commit/322679cd8d74f5992a10c842e867d427a083ef6a)

--- build-keepalived.sh
+++ build-keepalived.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2016-2019 (c) Yousong Zhou
+# Copyright 2016-2020 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=keepalived
-PKG_VERSION=2.0.19
+PKG_VERSION=2.0.20
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://www.keepalived.org/software/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=df670e0904d4e48e72ccc8409ad9c6de
+PKG_SOURCE_MD5SUM=a5966e8433b60998709c4a922a407bac
 PKG_DEPENDS='libnl3 net-snmp openssl'
 PKG_PLATFORM=linux
 
