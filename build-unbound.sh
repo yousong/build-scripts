a8d9d6464c0ff1b88a0b13851b5af5c615e53712
Yousong Zhou <yszhou4tech@gmail.com>
2020-07-31 02:35:05 +0800

    unbound: bump to version 1.11.0

--- build-unbound.sh
+++ build-unbound.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2016-2017 (c) Yousong Zhou
+# Copyright 2016-2020 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -9,10 +9,10 @@
 #
 # unbound-1.5.7 cannot compile with nettle-3.2 as the ssl library.
 PKG_NAME=unbound
-PKG_VERSION=1.5.7
+PKG_VERSION=1.11.0
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://unbound.net/downloads/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=a1253cbbb339dbca03404dcc58365d71
+PKG_SOURCE_MD5SUM=528dcf9bb9aa693a14f9ad5bab417b85
 PKG_DEPENDS='expat libevent openssl'
 
 . "$PWD/env.sh"
