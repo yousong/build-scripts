c070ef7db5f6b1d18c798490033333dae89fd983
Yousong Zhou <yszhou4tech@gmail.com>
2020-07-31 02:49:39 +0800

    curl: bump to version 7.71.1

--- build-curl.sh
+++ build-curl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2015-2019 (c) Yousong Zhou
+# Copyright 2015-2020 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -11,10 +11,10 @@
 # @openssl for SSL support
 #
 PKG_NAME=curl
-PKG_VERSION=7.46.0
+PKG_VERSION=7.71.1
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_SOURCE_URL="http://curl.haxx.se/download/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=9979f989a2a9930d10f1b3deeabc2148
+PKG_SOURCE_MD5SUM=7c681ac816491ded4a11814ebc717734
 PKG_DEPENDS='c-ares nghttp2 rtmpdump openssl zlib'
 
 . "$PWD/env.sh"
