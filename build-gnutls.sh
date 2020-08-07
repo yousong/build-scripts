87e46be307329dcc5e39af1bc7d66c0bdb40af74
Yousong Zhou <yszhou4tech@gmail.com>
2020-08-07 17:59:07 +0800

    gnutls: bump to version 3.6.11

--- build-gnutls.sh
+++ build-gnutls.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2020 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=gnutls
-PKG_VERSION=3.4.9
+PKG_VERSION=3.6.14
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_SOURCE_URL="ftp://ftp.gnutls.org/gcrypt/gnutls/v${PKG_VERSION%.*}/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=1b3b6d55d0e2b6d01a54f53129f1da9b
+PKG_SOURCE_MD5SUM=bf70632d420e421baff482247f01dbfe
 PKG_DEPENDS='gmp libtasn1 nettle p11-kit unbound'
 
 . "$PWD/env.sh"
@@ -20,8 +20,11 @@ PKG_DEPENDS='gmp libtasn1 nettle p11-kit unbound'
 #
 # Enable local libopts in src/libopts because libopts25 available in Debian
 # Wheezy lacks optionAlias() and will cause build failure
+#
+# Libunistring was not found. To use the included one, use --with-included-unistring
 CONFIGURE_ARGS+=(
 	--with-unbound-root-key-file="$INSTALL_PREFIX/etc/unbound/root.key"
+	--with-included-unistring
 	--enable-local-libopts
 	--disable-guile
 	--disable-silent-rules
--- build-libtasn1.sh
+++ build-libtasn1.sh
@@ -1,14 +1,35 @@
 #!/bin/bash -e
 #
-# Copyright 2016 (c) Yousong Zhou
+# Copyright 2016-2020 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 PKG_NAME=libtasn1
-PKG_VERSION=4.7
+PKG_VERSION=4.16.0
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://ftp.gnu.org/gnu/libtasn1/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=12d10ca4ae0a3b95f7aa06a076da39ec
+PKG_SOURCE_MD5SUM=531208de3729d42e2af0a32890f08736
+PKG_AUTOCONF_FIXUP=1
 
 . "$PWD/env.sh"
+
+do_patch() {
+	cd "$PKG_SOURCE_DIR"
+	patch -p0 <<"EOF"
+
+  corpus2array.c:123:4: error: 'for' loop initial declarations are only allowed in C99 mode
+
+--- Makefile.am.orig	2020-08-07 17:19:30.743500447 +0800
++++ Makefile.am	2020-08-07 17:19:36.051491824 +0800
+@@ -28,7 +28,7 @@ EXTRA_DIST = windows/asn1-parser/asn1-pa
+ 	CONTRIBUTING.md cfg.mk maint.mk AUTHORS NEWS ChangeLog		\
+ 	THANKS LICENSE
+ 
+-SUBDIRS = lib src fuzz tests
++SUBDIRS = lib src tests
+ 
+ if ENABLE_DOC
+ SUBDIRS += doc examples
+EOF
+}
