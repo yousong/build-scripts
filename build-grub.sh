1c5232437e29cca36eb5772176488be7df893b30
Yousong Zhou <yszhou4tech@gmail.com>
2020-09-11 16:07:10 +0800

    grub: initial version 2.04

---
+++ build-grub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+#
+# Copyright 2020 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=grub
+PKG_VERSION=2.04
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SOURCE_URL="https://ftp.gnu.org/gnu/$PKG_NAME/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=5aaca6713b47ca2456d8324a58755ac7
+
+. "$PWD/env.sh"
