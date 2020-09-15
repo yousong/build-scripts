4e2afe57d04ff4560ebddaad840758e48a1d2905
Yousong Zhou <yszhou4tech@gmail.com>
2020-09-15 22:35:05 +0800

    grub: notes for configuring for x86_64-efi

--- build-grub.sh
+++ build-grub.sh
@@ -12,3 +12,19 @@ PKG_SOURCE_URL="https://ftp.gnu.org/gnu/$PKG_NAME/$PKG_SOURCE"
 PKG_SOURCE_MD5SUM=5aaca6713b47ca2456d8324a58755ac7
 
 . "$PWD/env.sh"
+
+grub_amd64_efi() {
+	CONFIGURE_ARGS+=(
+		--with-platform=efi
+		--target=x86_64
+	)
+}
+
+grub_i386_pc() {
+	CONFIGURE_ARGS+=(
+		--with-platform=pc
+		--target=i386
+	)
+}
+
+#grub_amd64_efi
