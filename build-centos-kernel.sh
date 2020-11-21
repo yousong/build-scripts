220c46522359d70aec90d738e144511518997a4c
Yousong Zhou <yszhou4tech@gmail.com>
2020-11-21 11:59:46 +0800

    centos-kernel: bump to version 3.10.0-1160.6.1

--- build-centos-kernel.sh
+++ build-centos-kernel.sh
@@ -22,7 +22,7 @@
 #
 centos_major="${centos_major:-7}"
 centos_kernel_buildid="${centos_kernel_buildid:-.bs}"
-centos_kernel_buildid=.yn20191203
+centos_kernel_buildid=.yn20201125
 if [ "${centos_kernel_buildid#.}" = "${centos_kernel_buildid}" ]; then
 	echo "bad \$centos_kernel_buildid: must start with a dot" >&2
 	false
@@ -31,7 +31,7 @@ fi
 case "$centos_major" in
 	7)
 		centos_kernel_mainline_version=3.10.0
-		centos_kernel_revision_default=1062.4.3.el7
+		centos_kernel_revision_default=1160.6.1.el7
 		centos_kernel_revision="${centos_kernel_revision:-$centos_kernel_revision_default}"
 		centos_kernel_version="$centos_kernel_mainline_version-$centos_kernel_revision"
 		;;
@@ -55,7 +55,7 @@ else
 	PKG_SOURCE="kernel-$PKG_VERSION.src.rpm"
 	PKG_SOURCE_URL="http://vault.centos.org/centos/$centos_major/updates/Source/SPackages/$PKG_SOURCE"
 	: PKG_SOURCE_URL="http://vault.centos.org/centos/$centos_major/os/Source/SPackages/$PKG_SOURCE"
-	PKG_SOURCE_MD5SUM=361e1129ce338348f3daffba083297b5
+	PKG_SOURCE_MD5SUM=64289db233697a9f797b0053bc364449
 fi
 
 PKG_BUILD_DIR_BASENAME="$PKG_NAME-$PKG_VERSION"
@@ -139,22 +139,20 @@ do_patch() {
 	patch -p1 <<"EOF"
 --- a/kernel.spec.orig	2019-10-16 11:03:06.163470259 +0000
 +++ b/kernel.spec	2019-10-16 11:04:15.509438768 +0000
-@@ -453,6 +453,9 @@ Patch1000: debrand-single-cpu.patch
+@@ -453,6 +453,8 @@ Patch1000: debrand-single-cpu.patch
  Patch1001: debrand-rh_taint.patch
  Patch1002: debrand-rh-i686-cpu.patch
  
 +Patch2000: fix-building-nbd.patch
-+Patch2001: KVM-x86-Allow-suppressing-prints-on-RDMSR-WRMSR-of-u.patch
 +
  BuildRoot: %{_tmppath}/kernel-%{KVRA}-root
  
  %description
-@@ -796,6 +798,9 @@ ApplyOptionalPatch debrand-single-cpu.pa
+@@ -796,6 +798,8 @@ ApplyOptionalPatch debrand-single-cpu.pa
  ApplyOptionalPatch debrand-rh_taint.patch
  ApplyOptionalPatch debrand-rh-i686-cpu.patch
  
 +ApplyOptionalPatch fix-building-nbd.patch
-+ApplyOptionalPatch KVM-x86-Allow-suppressing-prints-on-RDMSR-WRMSR-of-u.patch
 +
  # Any further pre-build tree manipulations happen here.
  
@@ -189,90 +187,6 @@ Ref: https://lists.centos.org/pipermail/centos/2017-October/167060.html
  		nbd_cmd(&sreq) = NBD_CMD_DISC;
  
  		/* Check again after getting mutex back.  */
-EOF
-
-	cat >KVM-x86-Allow-suppressing-prints-on-RDMSR-WRMSR-of-u.patch <<"EOF"
-Upstream fab0aa3b776f0a3af1db1f50e04f1884015f9082
-
-  Subject: [PATCH] KVM: x86: Allow suppressing prints on RDMSR/WRMSR of
-   unhandled MSRs
-
-  Some guests use these unhandled MSRs very frequently.
-  This cause dmesg to be populated with lots of aggregated messages on
-  usage of ignored MSRs. As ignore_msrs=true means that the user is
-  well-aware his guest use ignored MSRs, allow to also disable the
-  prints on their usage.
-
-  An example of such guest is ESXi which tends to access a lot to MSR
-  0x34 (MSR_SMI_COUNT) very frequently.
-
-  In addition, we have observed this to cause unnecessary delays to
-  guest execution. Such an example is ESXi which experience networking
-  delays in it's guests (L2 guests) because of these prints (even when
-  prints are rate-limited). This can easily be reproduced by pinging
-  from one L2 guest to another.  Once in a while, a peak in ping RTT
-  will be observed. Removing these unhandled MSR prints solves the
-  issue.
-
-  Because these prints can help diagnose issues with guests,
-  this commit only suppress them by a module parameter instead of
-  removing them from code entirely.
-
-  Signed-off-by: Eyal Moscovici <eyal.moscovici@oracle.com>
-  Reviewed-by: Liran Alon <liran.alon@oracle.com>
-  Reviewed-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
-  Signed-off-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
-  Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
-  [Changed suppress_ignore_msrs_prints to report_ignored_msrs - Radim]
-  Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>
-
---- a/arch/x86/kvm/x86.c.orig	2019-12-03 02:22:43.713015134 +0000
-+++ b/arch/x86/kvm/x86.c	2019-12-03 02:25:35.970851847 +0000
-@@ -106,6 +106,9 @@ EXPORT_SYMBOL_GPL(kvm_x86_ops);
- static bool __read_mostly ignore_msrs = 0;
- module_param(ignore_msrs, bool, S_IRUGO | S_IWUSR);
- 
-+static bool __read_mostly report_ignored_msrs = true;
-+module_param(report_ignored_msrs, bool, S_IRUGO | S_IWUSR);
-+
- unsigned int min_timer_period_us = 500;
- module_param(min_timer_period_us, uint, S_IRUGO | S_IWUSR);
- 
-@@ -2320,7 +2323,9 @@ int kvm_set_msr_common(struct kvm_vcpu *
- 		/* Drop writes to this legacy MSR -- see rdmsr
- 		 * counterpart for further detail.
- 		 */
--		vcpu_unimpl(vcpu, "ignored wrmsr: 0x%x data %llx\n", msr, data);
-+		if (report_ignored_msrs)
-+			vcpu_unimpl(vcpu, "ignored wrmsr: 0x%x data 0x%llx\n",
-+				msr, data);
- 		break;
- 	case MSR_AMD64_OSVW_ID_LENGTH:
- 		if (!guest_cpuid_has(vcpu, X86_FEATURE_OSVW))
-@@ -2342,8 +2347,10 @@ int kvm_set_msr_common(struct kvm_vcpu *
- 				    msr, data);
- 			return 1;
- 		} else {
--			vcpu_unimpl(vcpu, "ignored wrmsr: 0x%x data %llx\n",
--				    msr, data);
-+			if (report_ignored_msrs)
-+				vcpu_unimpl(vcpu,
-+					"ignored wrmsr: 0x%x data 0x%llx\n",
-+					msr, data);
- 			break;
- 		}
- 	}
-@@ -2564,7 +2571,9 @@ int kvm_get_msr_common(struct kvm_vcpu *
- 					       msr_info->index);
- 			return 1;
- 		} else {
--			vcpu_unimpl(vcpu, "ignored rdmsr: 0x%x\n", msr_info->index);
-+			if (report_ignored_msrs)
-+				vcpu_unimpl(vcpu, "ignored rdmsr: 0x%x\n",
-+					msr_info->index);
- 			msr_info->data = 0;
- 		}
- 		break;
 EOF
 }
 
