2425de572e8b02867bf793b940820f7f0b2a3b0a
Yousong Zhou <yszhou4tech@gmail.com>
2021-10-15 02:45:30 +0000

    capnproto: initial version 0.9.1

---
+++ build-capnproto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+#
+# Copyright 2021 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=capnproto
+PKG_VERSION=0.9.1
+PKG_SOURCE="$PKG_NAME-c++-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://capnproto.org/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=4e86cda4bd894c94b1ac96bf3c447b8e
+
+. "$PWD/env.sh"
