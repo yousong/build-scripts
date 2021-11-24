3da7632291ea30ead41d32d7276170453e308544
Yousong Zhou <yszhou4tech@gmail.com>
2021-11-24 06:39:13 +0000

    tcpdump: bump to version 4.9.3

--- build-tcpdump.sh
+++ build-tcpdump.sh
@@ -1,15 +1,22 @@
 #!/bin/bash -e
 #
-# Copyright 2016-2017 (c) Yousong Zhou
+# Copyright 2016-2021 (c) Yousong Zhou
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
+# tcpdump 4.99 can fail with "[Invalid header: len(100) < caplen(116)]"
+# when reading dumps written by old versions of the program (e.g.  4.9.2).
+# The check was introduced in tcpdump commit 9e6ba479 ("Add sanity checks on
+# packet header (packet length / capture length)")
+#
+# - https://github.com/the-tcpdump-group/tcpdump/commit/9e6ba479d8cee861a396cae59d7cf91bd3a5a563
+#
 PKG_NAME=tcpdump
-PKG_VERSION=4.9.0
+PKG_VERSION=4.9.3
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_URL="http://www.tcpdump.org/release/$PKG_SOURCE"
-PKG_SOURCE_MD5SUM=2b83364eef53b63ca3181b4eb56dab0c
+PKG_SOURCE_MD5SUM=a4ead41d371f91aa0a2287f589958bae
 PKG_DEPENDS='libpcap openssl'
 
 . "$PWD/env.sh"
