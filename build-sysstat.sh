07db595e3303a00128e696cdcaf0cde7b3a020ec
Yousong Zhou <yszhou4tech@gmail.com>
2021-12-03 06:43:48 +0000

    sysstat: support building static binaries

--- build-sysstat.sh
+++ build-sysstat.sh
@@ -10,6 +10,9 @@
 #  - svctm was removed
 #  - %util can be misleading for devices capable of parallel ops
 #
+# - Matrix of activities, http://sebastien.godard.pagesperso-orange.fr/matrix.html
+# - Documentation, http://sebastien.godard.pagesperso-orange.fr/documentation.html
+#
 PKG_NAME=sysstat
 PKG_VERSION=12.3.1
 PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.xz"
@@ -25,4 +28,8 @@ CONFIGURE_ARGS+=(
 CONFIGURE_VARS+=(
 	conf_dir="$INSTALL_PREFIX/etc/sysconfig"
 	sa_dir="$INSTALL_PREFIX/var/log/sa"
+)
+
+MAKE_VARS+=(
+	LFLAGS="${EXTRA_LDFLAGS[*]} -static"
 )
