317adb1e2fc43fabf989712a90df8ddfb9847621
Yousong Zhou <yszhou4tech@gmail.com>
2022-01-12 19:42:59 +0800

    sshpass: initial version 1.09

---
+++ build-sshpass.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+#
+# Copyright 2022 (c) Yousong Zhou
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+PKG_NAME=sshpass
+PKG_VERSION=1.09
+PKG_SOURCE="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_URL="https://downloads.sourceforge.net/sshpass/$PKG_SOURCE"
+PKG_SOURCE_MD5SUM=191a9128a74d81ae36744d5deb50d164
+
+. "$PWD/env.sh"
